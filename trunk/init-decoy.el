b99c3e3c8d47a020938e5f4c191fa03c2868029e
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2007-12-14 17:57:34 +0000

    getting closer to finding this fucker
    git-svn-id: svn://localhost/mattie-subversion@477 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/init-decoy.el
+++ trunk/init-decoy.el
@@ -5,10 +5,19 @@
 ;; the entire purpose of this file is to decoy any attempts by customize
 ;; to destroy my code.
 
-(defun custom-save-variables ()
+(defun backtrace-on-custom ()
   (message "!CRITICAL! trapped attempt to modify emacs initialization file")
 
   (with-current-buffer "*Messages*"
     (backtrace)))
+
+(defun custom-save-all ()
+  (backtrace-on-custom))
+
+(defun custom-save-faces ()
+  (backtrace-on-custom))
+
+(defun custom-save-variables ()
+  (backtrace-on-custom))
 
 (load-file (concat (getenv "HOME") "/system/emacs/emacs.el"))
