574e36e2db65fbc183594177226a51831e3b6312
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2007-12-16 07:32:15 +0000

    document the init-decoy.el file a bit more
    git-svn-id: svn://localhost/mattie-subversion@488 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/init-decoy.el
+++ trunk/init-decoy.el
@@ -2,8 +2,14 @@
 ;; .emacs.el
 ;;----------------------------------------------------------------------
 
-;; the entire purpose of this file is to decoy any attempts by customize
-;; to destroy my code.
+;; Any of the functions custom- are subject to rewrite by customize.
+;; This is not optimal for me as I am still migrating a few things
+;; over to setq.
+
+;; When this work is finished the better approach would be to put
+;; the customization settings in .emacs.d . Until that is complete
+;; this file protects my config from rewrites by essentially disabling
+;; any customization modifications.
 
 (setq custom-file "/dev/null")
 
@@ -12,6 +18,8 @@
 
   (with-current-buffer "*Messages*"
     (backtrace)))
+
+;; This is a good canidate for defadvise.
 
 (defun custom-save-customized ()
   (backtrace-on-custom))
@@ -25,4 +33,5 @@
 (defun custom-save-variables ()
   (backtrace-on-custom))
 
+;; load the real emacs file.
 (load-file (concat (getenv "HOME") "/system/emacs/emacs.el"))
