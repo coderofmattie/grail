00755c20cd0bce2dae11a9c1ea8ec042aa7f19ab
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2007-12-31 09:52:01 +0000

    turn on flyspell for elisp
    git-svn-id: svn://localhost/mattie-subversion@537 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/spell.el
+++ trunk/spell.el
@@ -11,6 +11,10 @@
 (require 'flyspell)
 
 (setq-default
-  aspell-program-name "aspell")               ;; use aspell.
+  aspell-program-name "aspell"                     ;; use aspell.
+  flyspell-issue-message-flag nil)                 ;; don't bog down in bad english.
+
+(add-hook 'cperl-mode-hook 'flyspell-prog-mode)         ;; works quite nicely with cperl.
+(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)    ;; works nicely as well with elisp
 
 
