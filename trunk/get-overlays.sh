2d66ac8c02c4e53c48ec14d7747c9755d1949e78
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-01-01 15:04:05 +0000

    make sure the directory is present first
    git-svn-id: svn://localhost/mattie-subversion@558 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/get-overlays.sh
+++ trunk/get-overlays.sh
@@ -2,4 +2,5 @@
 
 dir="elisp/"
 url="http://www.dr-qubit.org/download.php?file=predictive/auto-overlays-0.9.1.tar.gz&counter=auto-overlays.tar.gz"
+test -d $dir || mkdir $dir
 cd $dir && wget -O - $url | tar xzf -
