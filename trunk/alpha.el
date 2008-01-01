2b6aaf1afbbb5baaaf2ad5c186c927b13da5344a
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-01-01 10:23:43 +0000

    add the inquisitio search function. An early experiment at managing deployment of a configuration from the inside of emacs.
    git-svn-id: svn://localhost/mattie-subversion@549 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/alpha.el
+++ trunk/alpha.el
@@ -94,6 +94,37 @@
 ;; experimental - interesting
 ;;----------------------------------------------------------------------
 
+(defun deploy-query-inquisitio-search ( package )
+  (list "-s" package)
+  )
+
+(defun deploy-search-inquisitio ( package )
+  "search for an Emacs package with Paludis's inquisitio search tool."
+  (interactive "MPackage? ")
+
+  (lexical-let
+    ((search-buffer (generate-new-buffer "deploy-inquisitio")))
+
+    (with-current-buffer search-buffer
+      (unless (= 0 (apply 'call-process "inquisitio"     ;; search program
+                     nil                                 ;; no stdin
+                     (list (current-buffer) nil)         ;; discard stderr , stdout -> current-buffer
+                     nil                                 ;; don't refresh
+
+                     "--category" "app-emacs"            ;; without this constraint inquisitio
+                                                         ;; is slow to moribund.
+                     (deploy-query-inquisitio-search package) ;; construct search arguments
+                     ))
+        )
+
+        ;; when we kill the buffer get rid of the window associated so the user
+        ;; doesn't have to tediously clean-up.
+        (add-hook 'kill-buffer-hook 'rid-window t t)
+      )
+
+    (pop-to-buffer search-buffer)
+    ))
+
 ;;----------------------------------------------------------------------
 ;; bounds-scan. Scan for the bounds indicated by delimiters. This
 ;; is aimed at code so allow ignored nested delimiters.
