4338aa792465de96387db4fae3d501ec9a1019a8
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-01-06 13:04:01 +0000

    all kinds of changes, signficantly have a hacked up test framework for the compilation phase
    git-svn-id: svn://localhost/mattie-subversion@592 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/test-parser.el
+++ trunk/test-parser.el
@@ -17,6 +17,10 @@
         "test"
         "the right thing"))))
 
+;;----------------------------------------------------------------------
+;; token interp phase
+;;----------------------------------------------------------------------
+
 (defmacro test-interp-token ( &rest syntax )
   (lexical-let
     ((compile (catch 'syntax-error
@@ -33,10 +37,42 @@
 (defun run-test-interp-token ( match-function )
   (lexical-let
     ((result (funcall match-function)))
-    (message "match? %s AST %s" (if (car result) "yes" "no") (pp (cdr result)))
+    (message "TI match? %s AST %s" (if (car result) "yes" "no") (pp (cdr result)))
     ))
 
 (defun run-test-token ()
   (interactive)
   (run-test-interp-token (test-interp-token token whitespace "[[:blank:]]+")))
 
+;;----------------------------------------------------------------------
+;; token compile phase
+;;----------------------------------------------------------------------
+
+;; this is the top-level of the compiler without the recursion and other
+;; complexity to isolate a specific component for testing.
+(defmacro test-compile ( test-syntax )
+  (let
+    ((match-table (make-vector parser-mtable-init-size 0)))
+
+    `(defun run-test-token ()
+       (interactive)
+       (let*
+         ((parser-position (cons (point) nil)) ;; initialize parser-position to the cursor.
+           (test-match ,(parser-compile-token (cdr test-syntax)))
+           (test-result (funcall test-match)))
+
+         (message "TC match? %s AST %s" (if (car test-result) "yes" "no") (pp (cdr test-result)))
+         ))
+  ))
+
+
+(test-compile
+  (token whitespace "[[:blank:]]*"))
+
+;; will need a test-comp-token that defines the match-table so that I can
+;; test the token compiler independently.
+
+(parser-compile
+  (token whitespace "[[:blank:]]*")
+  (token word "[[:alpha:]]+")
+  (term whitespace? word whitespace?))
