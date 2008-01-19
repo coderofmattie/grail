dd6b30e5fdb581c1dc8a2b45cb712ffc83c474bf
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-01-19 02:44:05 +0000

    try and increase the delay to reduce the flicker during fast scrolling, not working so far. Also make a note about eldoc
    git-svn-id: svn://localhost/mattie-subversion@702 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/styles/paren.el
+++ trunk/styles/paren.el
@@ -7,10 +7,14 @@
 
 (paren-activate)
 
+;; eldoc can be a useful minor mode. It displays the function signature
+;; documentation on a idle timer. I find it to be too distracting for
+;; little gain over c-h-f.
+
 (setq
   paren-showing t
   show-paren-style 'parenthesis
-  show-paren-delay nil
+  show-paren-delay 2
   paren-sexp-mode 'match )
 
 
