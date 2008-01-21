1f3efa11f20b10c3e819091078d90f056fd070ad
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-01-21 02:01:31 +0000

    add terminate-sequence which as I suspected fixes my problems. The pretty printer is basically hopeless, or my code is screwed up. Only the traverse will tell the tale.
    git-svn-id: svn://localhost/mattie-subversion@727 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/elisp/mattie-elisp.el
+++ trunk/elisp/mattie-elisp.el
@@ -5,6 +5,27 @@
 ;; Description: basic elisp programming tools.
 ;;----------------------------------------------------------------------
 
+(defun terminate-sequence ( &rest args )
+  "terminate sequence takes a all types concatenates into a list properly handling unterminated sequences"
+  (lexical-let
+    ((terminated nil))
+
+    (dolist (arg (reverse args))
+      (if (consp arg)
+        (lexical-let
+          ((reverse-stack nil)
+           (sequence arg))
+
+          (while (consp sequence)
+            (push (car sequence) reverse-stack)
+            (setq sequence (cdr sequence)))
+
+          (if sequence (push sequence reverse-stack))
+          (setq terminated (append (reverse reverse-stack) terminated)))
+
+        (setq terminated (cons arg terminated)) ))
+    terminated))
+
 (defun terminated-list-p ( list )
   "return true only if the list is nil terminated"
   (if (consp list)
