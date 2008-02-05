fba43e87cdc2569969dfb3f4e43be81707b426df
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-02-05 07:37:33 +0000

    remove the unterminated list experiments from mattie-elisp
    git-svn-id: svn://localhost/mattie-subversion@782 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/elisp/mattie-elisp.el
+++ trunk/elisp/mattie-elisp.el
@@ -5,46 +5,6 @@
 ;; Description: basic elisp programming tools.
 ;;----------------------------------------------------------------------
 
-(defun join-cons ( a b )
-  "like cons but joins as a list instead of nesting"
-  (let
-    ((new-a (if (cdr a) (cons a nil) a))
-     (new-b (if (cdr b) (cons b nil) b)) )
-    (setcdr new-a new-b)
-    new-a))
-
-(defun terminate-sequence ( &rest args )
-  "terminate sequence takes a all types concatenates into a list properly handling unterminated sequences"
-  (lexical-let
-    ((terminated nil))
-
-    (dolist (arg (reverse args))
-      (if (and (consp arg) (not (eq 'quote (car arg))))
-        (lexical-let
-          ((reverse-stack nil)
-            (sequence arg))
-
-          (while (consp sequence)
-            (push (car sequence) reverse-stack)
-            (setq sequence (cdr sequence)))
-
-          (if sequence (push sequence reverse-stack))
-          (setq terminated (append (reverse reverse-stack) terminated)))
-
-        (setq terminated (cons arg terminated)) ))
-    terminated))
-
-(defun terminated-list-p ( list )
-  "return true only if the list is nil terminated"
-  (if (consp list)
-    (lexical-let
-      ((element (cdr list)))
-
-      (while (consp element)
-        (setq element (cdr element)))
-
-      (eq nil element)) ))
-
 ;; required for my patched em-unix, note: merged upstream, may collide
 ;; on a update.
 (defun nil-blank-string ( string )
