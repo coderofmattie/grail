e29c8a93026883a65b0a947072c72d548cc90042
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-02-11 16:29:29 +0000

    remove a bunch of dead comments from broken.el concerning the parser. What is left is a TODO list with notes.
    git-svn-id: svn://localhost/mattie-subversion@798 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/broken.el
+++ trunk/broken.el
@@ -4,93 +4,39 @@
 ;; and fixing.
 ;;----------------------------------------------------------------------
 
+;; CURRENT
+
+;; I don't have a return phase, it's currently split imbetween the logic phase,
+;; the lexical scope, and the generator. I think I need to separate the return
+;; phase. The possibility of an operator going in to the return phase is significant.
+
+;; making lambda optional for the parser entry point
+
+;; optional returns match success when a match fails.
+
+;; where does the stuff like optional ? the matching logic go ? before or
+;; after the conditional ? putting the logic before the conditional could
+;; seriously screw up things like backtracking, but maybe that is a good thing.
+
+;; the logic should influence ast consumption. otherwise the grammar would
+;; possibly have semantics that do not paralell the definition. This is
+;; actually a real bug I have sniffed out here, since it is currently in
+;; the conditional which is wrong. Replace transform with logic.
+
+
+;; I am looking at a higher level than the code generation switches. I
+;; am looking at my three parser effects, match effect, logical
+;; effect, and ast effect. I also need a return effect.
+
 ;;----------------------------------------------------------------------
 ;; old stuff.
 ;;----------------------------------------------------------------------
 
-;; Is a functional closure runs in the lexical scope useful ?  the ast
-;; append will be fine I think, but the AST generation is no longer
-;; controlled by the branching logic. I can't see anything that
-;; couldn't be accomplished by nesting except violating semantics.
-
-;; Any closure stacking should result in function stacking. the point
-;; of the dynamic scope is largely to show the parse tree with it's tail.
-
-
-;; how do I implement attaching a node instead of merging a node ? this
-;; is what descend is all about. ast-attach, ast-merge. Probably only
-;; need ast-attach as an override. ast-node for attach or ast-leaves for merge
-
-;; ast-node-attach sounds best, like ast-node-merge, object then verb.
-;; ast-newroot is actually a interesting thought. along with being a root, it's
-;; a place where data is injected into the parse tree. This is freely controlled
-;; in tokens, but not in productions which are limited to symbols. in reality
-;; it should be called ast-new-production.
-
-;; ast-attach-production, by not stripping off the car under the assumption that
-;; the car is a symbol we use to identify the production, rather than a logical
-;; match status, we have the ability to create productions.
-
-;; if it isn't a production up front then we need to assume merging.
-
-;; also need the initial case, what if parse tree is not bound in the function scope ?
-;; or should it just always be bound ? the case of returning it without consuming it
-;; is the start case, so we are back to attach or return, depending on wether parse-tree is
+;; also need the initial case, what if parse tree is not bound in the
+;; function scope ?  or should it just always be bound ? the case of
+;; returning it without consuming it is the start case, so we are back
+;; to attach or return, depending on wether parse-tree is
 ;; bound. vunderbar.
-
-
-;; ast-new-production, ast-attach-production, ast-return-production
-;; okay with this table approach I need to know how it's modified. Each group that
-;; is exclusive to the other creates a tag. If the tag matches it overrides the
-;; default. Otherwise it signals an error.
-
-;; shadowing the tree, basically environment modification gives the immediate
-;; advantage of allowing the tail of the AST list to be tracked during parsing.
-;; The lexical environment has the head, the dynamic environment has the tail.
-;; This keeps the computational complexity of AST construction linear.
-
-;; This is why a function closure would not work all that well. It would need to
-;; dynamically scope a tail. best to just let it go.
-
-;; a predicate is a match function that produces a match result in the
-;; match environment.
-
-;; a match closure can be passed the predicate for delayed evaluation.
-;; this is typically used for greedy
-
-;; a closure is essentially an optional two level deep delayed evaluation.
-;; the two levels is required to directly express grouping/alternation and
-;; positive/kleene closure.
-
-;; matchahead discards the input consumption of a descent.
-
-;; forget  discards the AST construction of a descent.
-
-;; optional returns match success when a match fails.
-
-;; backtrack discards both input consumption on match failure or forgets
-;; a saved position on match success.
-
-;; ast-newroot creates a new AST tree populated by the parse descent
-;; that shadows the existing AST tree
-
-;; gen-transform is applied outside of any dynamic-scoping.
-;; effects and rvalue take place within a lexical-scope if any.
-
-;; here is what I am pondering. backtracking introduces conditionals.
-;; do conditionals affect all domains ? or can it be selected ?
-;; are consumption and ast possible to separate conditionally ?
-
-;; that make up ast is what I will need for the start thingy. Tokens
-;; are essentially the make up ast, which means of course that I do
-;; need mandatory consumption. What happens when I directly attach
-;; to the match result of a token ? that means that I absolutely
-;; need consumption in the match call.
-
-;; Focus. Function to function. both simply make up return values. Where is the consumption ?
-;; no where.
-
-;; FIXME: branching implies a trap.
 
 ;; I mean, what happens when a single token is the grammar ? it or reduces
 ;; down to a single token, and then what ? putting a consume in the match
@@ -102,8 +48,6 @@
 
 ;; NEXT STEP
 
-;; generate errors for intermediate forms with conflicting side effects
-
 ;; then write the simplification part, building a shift reduction that transforms
 ;; conflicts into nested functions. If the generated parser functions are essentially
 ;; referentially transparent by virtue of doing the right thing when nested then
@@ -113,116 +57,12 @@
 ;; dumping the intermediate table after all updates, and then the generated
 ;; function
 
-;; making lambda optional for the parser entry point
-
-;; If i convert the simple dynamic scope of the table, to a hash table
-;; that is dynamically scoped then I can make wrappers for query, setting,
-;; and inserting operations. That will be enough I think to detect conflicts.
-
-;; keeping the queries terse will be difficult, might ponder wrapping setting
-;; only. consumption will likely have to be seperated into a different entry.
-;; consumption outside of dynamic scope is a serious non-orthogonality. Other
-;; transforms are harmless and can be compounded.
-
-;; when will the function scope be restored ? the let match environment
-;; looks like a restore point, but caveat emperor, the backtracking doesn't
-;; restore it until func-rvalue. hmmm. This seams essential, so that the
-;; branching in the function can decide what to do with the residue of the
-;; match environment.
-
-;; transform may modify the match result, but it can't do anything like
-;; alter the input consumption of the match, that will happen in branching.
-
-;; where does the stuff like optional ? the matching logic go ? before or
-;; after the conditional ? putting the logic before the conditional could
-;; seriously screw up things like backtracking, but maybe that is a good thing.
-
-;; the logic should influence ast consumption. otherwise the grammar would
-;; possibly have semantics that do not paralell the definition. This is
-;; actually a real bug I have sniffed out here, since it is currently in
-;; the conditional which is wrong. Replace transform with logic.
-
-;; BIG IDEA: validation and generation need to be two separate steps.
-;; say you have a intermediate form in a table. you have a cycle of
-;; copy, merge,validate.  you keep your last validated intermediate
-;; form, when you fail to validate you kick out a function and
-;; nest. this way you are not generating code all the time. would
-;; maybe simplify a bit.
-
-;; I need a bind hash table function. lethash. I can even modify I think
-;; if I do it right. that gives full read/write.
-
-;; better yet use an association list. It's way better, easier to populate
-;; etc. Look up time doesn't need.
-
-;; ast-rvalue
-;; ast-attach
-;; ast-merge
-;; ast-return
-
-(always backtrack)
-(fail   backtrack)
-
-(ast tail 'start)   ;; creates a new ast tail initialized to 'start
-
-;;;; WOOO
-
-;; here we go, backtrack is a concept for ast as well !! including forget !!!
-
-
-(input backtrack)
-(input discard)
-
-(ast backtrack)
-(ast discard)
-
-(logic xor)
-(ast node 'start)    ;; give the node a initial value, causes parser-tree-add-node
-                     ;; to be 
-
-
-
-
-(func-ast)  ;; if there is a value here, ast-scope should pick it up,
-(match-ast) ;; this is where things get lexically scoped. Branching is not required just
-            ;; to shadow. A seperate variable is not required because the result
-            ;; will show up in production, so it can be initialized as a match result
-            ;; which would be merged, or func-ast-add could be 
-
-(always rvalue)
-(
-(match rvalue ast) ;; this triggers ast shadowing.
-
-;; recovery mechanism: create a parser function that is an or of all the terminals in the grammar.
-;; keep moving the input pointer by one until such a sync is found. In fact recover could just be
-;; an or of non-terminals.
-
-
+;; recovery mechanism: create a parser function that is an or of all
+;; the terminals in the grammar.  keep moving the input pointer by one
+;; until such a sync is found. In fact recover could just be an or of
+;; non-terminals.
 
 ;; FOCUS:
-
-;; need to focus on the validator. Make the descisians more transparent with
-;; some sort of defun breakdown, or tracing. Making the validator will turn
-;; all of these comments into code.
-
-;; how do I make it persistent ?
-
-;; custom code must be domain tagged so it can be validated. It can also be
-;; a user domain which doesn't collide.
-
-;; I am looking at a higher level than the code generation switches. I am looking
-;; at my three parser effects, match effect, logical effect, and ast effect. I
-;; also need a return effect.
-
-;; so I have forget, unconditionally discard ast, then I have conditional ast
-;; which is implies ast-new-root, but a wank like ast-new-root without conditional
-;; is doable actually. useful to delay ast consumption to the next function.
-
-;; any conditional branching of ast effect, needs to set ast-new-root, otherwise
-;; ast consumption will happen outside of conditional control.
-
-;; binding needs to happen seperate from merging so I can call myself with dependencies.
-;; 
 
 ;; The first phase is to simply build the validator and think out the
 ;; semantics properly. that should get me to a working generator. The
@@ -289,133 +129,9 @@
      function rvalue
    "
 
-  (lexical-let
-    ((param-type (make-hash-table)) )
-
-    ;; initialize the hash of parameters that are lists instead of single values.
-
-    (mapc (lambda ( list-name )
-            (puthash list-name t param-type))
-      '(match-before match-after gen-match-effects gen-fail-effects))
-
-    ;; generation table is dynamically scoped to ease breaking up
-    ;; the function generator into pieces and phases.
-
-    ;; This table represents the intermediate form of
-    ;; simplification.
-    (let
-      ((gen-sexp          nil)
-
-       ;; in application order top-down.
-       ;; gen-predicate match-list?
-       (gen-closure       nil)
-       (gen-consume       nil)
-       (gen-trap          nil)
-       (gen-transform     nil)
-
-        ;; gen production captures the match result to the lexically
-        ;; scoped production symbol.
-       (gen-production    nil)
-
-       (gen-ast-root      nil)
-       (gen-lexical-scope nil)
-       (gen-dynamic-scope nil)
-
-        ;; before/after hooks.
-       (gen-match-before  nil)
-       (gen-match-after   nil)
-
-        ;; branching on match results.
-
-       (gen-match-effects nil)
-       (gen-match-rvalue  nil)
-
-       (gen-fail-effects  nil)
-       (gen-fail-rvalue   nil))
-
-      (mapc (lambda (s)
-              (catch 'next
-                (unless s (throw 'next nil))
-
-                (if (listp s)
-                  (if (gethash (car s) param-type)
-                    (eval `(push ,(cadr s) ,(car s)))
-                    ;; FIXME: here is where the test will go for something already set.
-                    (eval `(setq ,(car s) ,(cadr s))))
-                  (cond
-                    ((eq s 'consume) (setq gen-consume t))
-                    ((eq s 'trap-fail) (setq gen-trap t))
-                    ;; I just triggered branching and failed to consume on the match
-                    ;; branch. not good.
-                    ((eq s 'optional)
-                      (setq  gen-fail-rvalue `(parser-make-production-match nil)))
-                    ((eq s 'forget)
-                      (progn
-                        (setq gen-match-rvalue '(parser-make-production-match nil))
-                        (push `(parse-tree (make-production-match nil)) gen-dynamic-scope)))
-                    ((eq s 'matchahead)
-                      (progn
-                        (push `(parser-push) gen-match-before)
-                        (push `(parser-backtrack)  gen-match-after)))
-                    ((eq s 'backtrack)
-                      (progn
-                        (push `(parser-push)       gen-match-before)
-                        (push `(parser-pop)        gen-match-effects)
-                        (push `(parser-backtrack)  gen-fail-effects)))
-                    ((eq s 'ast-newroot)
-                      (progn
-                        ;; values for gen-ast-root can be default, symbol,
-                        ;; or sexp initializer.
-                        (if gen-ast-root
-                          (if (symbolp gen-ast-root)
-                            (setq gen-ast-root `(cons ',gen-ast-root nil)))
-                          (setq gen-ast-root `(parser-make-logical-match)))
-
-                        (push `(ast-root ,gen-ast-root)  gen-lexical-scope)
-                        (push `(parse-tree ast-root)     gen-dynamic-scope)
-                        (unless gen-match-rvalue
-                          ;; since our shadowing has detached our AST construction
-                          ;; be sure to attach it. as long as it was not initialized
-                          ;; with a number.
-                          (setq gen-match-rvalue `(parser-?consume-match ast-root)))))
-                    )))) statements)
-
-      (if (and
-            (parser-gen-branch-p)
-            (not gen-trap))
-        (setq gen-trap t))
-
-      ;; What I am really looking for in in a symbol is wether it is merged
-      ;; as a leaf or a node, 
-
-      ;; when there is match after statements we always need to preserve
-      ;; the return value, so that it can be returned as the final value
-      ;; of the list.
-      (if (and gen-match-after (not gen-production))
-        (setq gen-production t))
-
-      ;; If there is a conditional branch the production will need to
-      ;; be referenced in two places so turn on production generation.
-      (if (and
-            (and
-              (parser-gen-branch-p)
-              (not gen-match-rvalue)) ;; optimize, when match-rvalue is specified
-                                      ;; we can turn off automatic production generation.
-
-            (not gen-production))
-        (setq gen-production t))
-
-      (funcall (if gen-sexp 'seq-filter-nil 'parser-gen-lambda)
-        gen-match-before
-
-        ;; can this sequence in name match the description of phases ?
-        (parser-gen-lexical-scope
-          (parser-gen-match-rvalue
-            (parser-gen-dynamic-scope
-              (parser-gen-match-call))))
-
-        gen-match-after
-        )) ))
+;;----------------------------------------------------------------------
+;; misc.
+;;----------------------------------------------------------------------
 
 ;; This would be very cool if I had a better font for greek symbols.
 
