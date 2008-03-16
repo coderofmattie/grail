dc86835251f84ece61208ba7b315f355634c700e
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-03-16 01:39:53 +0000

    remove the old closure implementation which is superseded by closure.el
    git-svn-id: svn://localhost/mattie-subversion@934 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/elisp/mattie-elisp.el
+++ trunk/elisp/mattie-elisp.el
@@ -49,6 +49,8 @@
 
 ;; I really like this implementation, would map-filter-nil benefit from
 ;; using consp ?
+
+;; could use remq 'nil list
 (defun list-filter-nil ( list )
   "Filter nil symbols from a list"
   (if (consp list)
@@ -77,99 +79,5 @@
      (put ',symbol
        'error-conditions (append '(error ,symbol) ',isa-list))
      (put ',symbol 'error-message ,message) ))
-
-;;----------------------------------------------------------------------
-;; closures
-;;----------------------------------------------------------------------
-
-;; these are experimental prototypes. The final versions need to be
-;; implemented at the C level.
-
-(defun scope-bind-closure ( closure body )
-  "traverse the tree depth first pre-binding any symbol found in closure."
-
-  (if (consp body)
-    (lexical-let
-      ((atom (car body)))
-
-      (cons
-        (if (listp atom)
-          (scope-bind-closure closure atom)
-
-          (if (symbolp atom)
-            (or
-              (intern-soft (symbol-name atom) closure)
-              atom)
-            atom))
-
-        (scope-bind-closure closure (cdr body))))
-    body))
-
-(defmacro save-lexical-closure ( closure &rest body )
-  "a persistent lexical binding. The objarray CLOSURE appears lexically
-   scoped in that a recursive traversal binds symbols of equal name
-   in CLOSURE. altering these pre-bound symbols with setq changes the
-   value in CLOSURE allowing the values to persist beyond the form in
-   objarray CLOSURE.
-
-   Currently this is a experimental hack so it incurs the cost
-   of a recursive pre-bind in addition to eval each time evaluated."
-  (declare (debug (symbolp body)))
-  `(eval (scope-bind-closure ,closure ',(cons 'progn body))))
-
-(defmacro use-dynamic-closure ( closure &rest body )
-  "use a saved closure as a dynamic scope with private copy."
-  (declare (debug (symbolp body)))
-  `(let
-     ,(lexical-let
-        ((bindings nil))
-        (mapatoms
-          (lambda ( s )
-            (push `(,(read (symbol-name s))
-                     (symbol-value (intern ,(symbol-name s) ,closure)))
-              bindings))
-          (eval closure))
-        bindings)
-     ,@body))
-
-(defun pp-closure ( closure )
-  "pretty print a closure returning a string."
-  (lexical-let
-    ((strings nil))
-
-    (mapatoms
-      (lambda ( s )
-        (push (format "symbol: %s = %s\n"
-                (symbol-name s)
-                (pp-to-string (symbol-value (intern (symbol-name s) closure)))) strings)) closure)
-    (apply 'concat strings)))
-
-(defun copy-closure ( closure )
-  "copy CLOSURE an objarray so that the values are not shared unlike copy-sequence."
-  (lexical-let
-    ((copy (make-vector (length closure) 0)))
-
-    (mapatoms
-      (lambda ( s )
-        (lexical-let
-          ((name (symbol-name s)))
-          (set (intern name copy) (symbol-value (intern name closure))))) closure)
-    copy))
-
-(defmacro make-closure ( &rest defines )
-  "create a symbol table initializing SYMBOL with eval'd VALUE"
-  (lexical-let
-    ((table (make-vector (length defines) 0)))
-
-    (mapc (lambda ( pair )
-            (set (intern (symbol-name (car pair)) table) (eval (cadr pair)))) defines)
-    table))
-
-(defun value-from-closure ( sym closure )
-  (symbol-value (intern (symbol-name sym) closure)))
-
-(defun symbol-from-closure ( sym closure )
-  (intern (symbol-name sym) closure))
-
 
 (provide 'mattie-elisp)
