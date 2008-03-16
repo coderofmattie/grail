3b48dfe6629fe86294c20727acce64b4d1dcb995
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-03-16 11:40:18 +0000

    move some functions from alpha into mattie-elisp as they are required by the parser, and are general purpose
    git-svn-id: svn://localhost/mattie-subversion@945 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/elisp/mattie-elisp.el
+++ trunk/elisp/mattie-elisp.el
@@ -11,6 +11,23 @@
   "if a string is all blanks return nil, if there are non-blank characters return the string"
   (if (string-match "[^[:blank:]]" string ) string))
 
+(defun eqn ( a b )
+  "eqn A B
+
+   A and B are equal if their symbol names are a string match.
+  "
+  (string-equal (symbol-name a) (symbol-name b)))
+
+(defun precise-list-p ( x )
+  "precise-list-p X
+
+   more precise listp predicate. Checks that x is both a cons,
+   and the cdr of x is a cons.
+  "
+  (and
+    (consp x)
+    (consp (cdr x))))
+
 (defun make-anon-func ( name sexp )
   "bind an un-evaluated anonymous function to an un-interned symbol"
   (let
@@ -79,5 +96,74 @@
      (put ',symbol
        'error-conditions (append '(error ,symbol) ',isa-list))
      (put ',symbol 'error-message ,message) ))
+
+(defun consume-list ( list consume )
+  "consume-list LIST CONSUME
+
+   use a consume function to consume a list. unlike mapc instead
+   of consuming only a single element of the list at a time the
+   head, and the tail are passed to CONSUME so that a single call
+   can consume a variable n elements from list.
+
+   This function amounts to a TCO kludge allowing the function to
+   be expressed in a recursive form."
+  (when list
+    (while (setq list (funcall consume (car list) (cdr list)))))
+  nil)
+
+(defun apply-n-times ( func n x )
+  "apply-n-times FUNC N X
+
+   apply FUNC to X N times, With X set to the return
+   value of FUNC for each iteration.
+  "
+  (while (> n 0)
+    (setq x (funcall func x))
+    (decf n))
+  x)
+
+(defun split-list ( n list )
+  "split-list N LIST
+   return a cons of LIST split into two lists at index N.
+  "
+  (if (> n 1)
+    (lexical-let
+      ((a-list list)
+        (b-list nil)
+        (before-split (apply-n-times 'cdr (- n 1) list)))
+
+      (setq b-list (cdr before-split))
+      (setcdr before-split nil)
+
+      (cons a-list b-list))
+    (cons (cons (car list) nil) (cdr list))))
+
+;;----------------------------------------------------------------------
+;; tail iterator
+;;----------------------------------------------------------------------
+
+(defun tail-iterator-merge ( a b )
+  (setcdr a b)
+
+  (do ((x b))
+    ((null (cdr x)) x)
+    (setq x (cdr x))))
+
+(defun tail-iterator ( bind-to )
+  (set bind-to (cons nil nil))
+
+  (lexical-let
+    ((tail (symbol-value bind-to)))
+
+    (lambda ( x )
+      (if (precise-list-p x)
+        (setq tail (tail-iterator-merge tail x))
+        (progn
+          (setcdr tail (cons x nil))
+          (setq tail (cdr tail))) )) ))
+
+(defun tail-list ( list )
+  (cdr list))
+
 
 (provide 'mattie-elisp)
