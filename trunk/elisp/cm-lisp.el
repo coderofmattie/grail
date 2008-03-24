649b46cece814a8d57e64139178cd91769460660
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-03-24 07:06:15 +0000

    move the function-arity into cm-lisp.el
    git-svn-id: svn://localhost/mattie-subversion@979 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/elisp/cm-lisp.el
+++ trunk/elisp/cm-lisp.el
@@ -19,5 +19,40 @@
     (fset anon-func (eval sexp))
     anon-func))
 
+;;----------------------------------------------------------------------
+;; function-arity
+;;----------------------------------------------------------------------
+
+;; copied from the page:
+;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg00056.html
+;; Author: Kevin Rodgers, kevin.d.rodgers@gmail.com
+
+(require 'help-fns)
+
+(defun lambda-arity (function)
+  "Return minimum and maximum number of args allowed for FUNCTION.
+FUNCTION must be a symbol whose function binding is a lambda expression
+or a macro.
+The returned value is a pair (MIN . MAX).  MIN is the minimum number
+of args.  MAX is the maximum number or the symbol `many', for a lambda
+or macro with `&rest' args."
+  (let* ((arglist (help-function-arglist function))
+         (optional-arglist (memq '&optional arglist))
+         (rest-arglist (memq '&rest arglist)))
+    (cons (- (length arglist)
+             (cond (optional-arglist (length optional-arglist))
+                   (rest-arglist (length rest-arglist))
+                   (t 0)))
+          (cond (rest-arglist 'many)
+                (optional-arglist (+ (length arglist)
+                                     (length optional-arglist)
+                                     -1))
+                (t (length arglist))))))
+
+(defun function-arity ( function )
+  (if (subrp function)
+    (subr-arity function)
+    (lambda-arity function)))
+
 (provide 'cm-lisp)
 
