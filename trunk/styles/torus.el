bc7179d25c350b6d39d355a836328a5e5b673148
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-03-25 13:26:27 +0000

    this is better, the hook broke navigation of the torus so I pulled it, binding select to prompt-less current helps greatly
    git-svn-id: svn://localhost/mattie-subversion@991 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/styles/torus.el
+++ trunk/styles/torus.el
@@ -5,17 +5,13 @@
 
 (mtorus-enable-select-follows-choose)
 
-(add-hook 'mtorus-navigate-post-hook
-  (lambda (element)
-    (mtorus-select-element element)))
-
 (global-set-key (kbd "<M-right>")  'mtorus-next-element)
 (global-set-key (kbd "<M-left>")   'mtorus-prev-element)
 
 (global-set-key (kbd "<M-up>")     'mtorus-parent-element)
 (global-set-key (kbd "<M-down>")   'mtorus-child-element)
 
-(global-set-key (kbd "M-RET")      'mtorus-select-element)
+(global-set-key (kbd "M-RET")      'mtorus-select-current-element)
 
 
 
