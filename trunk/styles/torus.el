e3334785635616fbe63be36eb59fb6f30da188b2
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-03-25 13:05:48 +0000

    add the hook that makes it killer IMHO
    git-svn-id: svn://localhost/mattie-subversion@990 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/styles/torus.el
+++ trunk/styles/torus.el
@@ -5,6 +5,10 @@
 
 (mtorus-enable-select-follows-choose)
 
+(add-hook 'mtorus-navigate-post-hook
+  (lambda (element)
+    (mtorus-select-element element)))
+
 (global-set-key (kbd "<M-right>")  'mtorus-next-element)
 (global-set-key (kbd "<M-left>")   'mtorus-prev-element)
 
@@ -12,7 +16,6 @@
 (global-set-key (kbd "<M-down>")   'mtorus-child-element)
 
 (global-set-key (kbd "M-RET")      'mtorus-select-element)
-
 
 
 
