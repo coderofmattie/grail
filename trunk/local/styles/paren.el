508ce155c1cf1cfeaf4509543ea3ec76fd717b30
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-03-31 04:09:23 +0000

    new lisp structuring of the file
    git-svn-id: svn://localhost/mattie-subversion@1030 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/local/styles/paren.el
+++ trunk/local/styles/paren.el
@@ -1,15 +1,11 @@
 ;;----------------------------------------------------------------------
-;; paren.el
+;; lisp.el
+;; Primary Author: Mike Mattie
+;; Copyright:
 ;;----------------------------------------------------------------------
 (require 'mic-paren)
 
 ;; fancy paren/delimited highlighting.
-
-(paren-activate)
-
-;; eldoc can be a useful minor mode. It displays the function signature
-;; documentation on a idle timer. I find it to be too distracting for
-;; little gain over c-h-f.
 
 (setq
   paren-showing t
@@ -17,4 +13,57 @@
   show-paren-delay 2
   paren-sexp-mode 'match )
 
+(paren-activate)
+
+(defun swap-paren-keys ()
+  "bind the parentheses to the brace keys, while the shifted
+   paren keys become the braces."
+  (interactive)
+
+  ;; make the parentheses a bit easier to type, less shifting.
+  (local-set-key (kbd "[") (lambda () (interactive) (insert-char ?\( 1 nil)))
+  (local-set-key (kbd "]") (lambda () (interactive) (insert-char ?\) 1 nil)))
+
+  (local-set-key (kbd "(") (lambda () (interactive) (insert-char ?\[ 1 nil)))
+  (local-set-key (kbd ")") (lambda () (interactive) (insert-char ?\] 1 nil))) )
+
+(add-hook 'emacs-lisp-mode-hook
+  'swap-paren-keys)
+
+(add-hook 'emacs-lisp-mode-hook
+  (lambda ()
+    (tune-programming "elisp")
+
+    ;; this binding is very important. normal evaluation of defuns such as defvar
+    ;; and defcustom do not change the default value because the form only sets
+    ;; the value if nil.
+
+    ;; eval-defun will "reset" these forms as well as not echoing into the buffer.
+    ;; this function/keybinding should be used exclusively to avoid frustrating
+    ;; errors.
+    (local-set-key (kbd "C-x e") 'eval-defun)
+
+    ;; replace dired at point, far less useful to me than instrumenting a function.
+    (local-set-key (kbd "C-x d") 'edebug-defun)
+
+    ;; elisp doesn't need tags, find-function works just fine.
+    (local-set-key (kbd "M-.")   'find-function) ))
+
+;;----------------------------------------------------------------------
+;;
+;;----------------------------------------------------------------------
+
+(eval-after-load "dwim-tab"
+  (add-hook 'emacs-lisp-mode-hook
+    (lambda ()
+      (dwim-tab-localize 'lisp-complete-symbol))))
+
+(eval-after-load "erc"
+  (progn
+    (setq
+      erc-default-server "irc.freenode.net"
+      erc-default-port "6667"
+      erc-nick "codermattie")
+
+    (add-hook 'erc-mode-hook 'swap-paren-keys) ))
 
