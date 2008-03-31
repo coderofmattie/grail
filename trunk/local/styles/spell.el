2f2c4d122f1db1476752a060112f6c2a427e780d
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-03-31 05:03:39 +0000

    use dwim-tab-add-context
    git-svn-id: svn://localhost/mattie-subversion@1043 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/local/styles/spell.el
+++ trunk/local/styles/spell.el
@@ -9,11 +9,11 @@
   ispell-program-name "aspell"                     ;; use aspell.
   flyspell-issue-message-flag nil)                 ;; don't bog down in bad english.
 
-(add-hook 'text-mode-hook 'flyspell-mode)          ;; turn on regular flyspell mode text
-                                                   ;; mode buffers.
+(add-hook 'text-mode-hook       'flyspell-mode)          ;; turn on regular flyspell mode text
+                                                         ;; mode buffers.
 
-(add-hook 'cperl-mode-hook 'flyspell-prog-mode)         ;; works quite nicely with cperl.
-(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)    ;; works nicely as well with elisp
+(add-hook 'cperl-mode-hook      'flyspell-prog-mode)     ;; works quite nicely with cperl.
+(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)     ;; works nicely as well with elisp
 
 (defun correct-over-flyspell ()
   "auto-correct the word if over a flyspell region, return t only
@@ -27,7 +27,10 @@
 
 ;; create a tab context where tab will invoke flyspell-auto-correct-word
 ;; at the point.
-(setq tab-context (cons 'correct-over-flyspell tab-context))
+
+(dwim-tab-add-context 'correct-over-flyspell)
+
+
 
 
 
