f9fbe16ee3721315bdd0ee7052e9dc024d793fdb
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-04-01 08:33:59 +0000

    more secret sauce, works on linux now
    git-svn-id: svn://localhost/mattie-subversion@1089 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/dist/get-cedet.sh
+++ trunk/dist/get-cedet.sh
@@ -4,6 +4,8 @@ cd elisp
 
 if test -d cedet ; then
   cd cedet && cvs update
+  touch `find -name Makefile -print`
+  make
 else
   cvs -d:pserver:anonymous@cedet.cvs.sourceforge.net:/cvsroot/cedet login
   cvs -z3 -d:pserver:anonymous@cedet.cvs.sourceforge.net:/cvsroot/cedet co -P cedet
