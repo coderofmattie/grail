3eb08b6c1f6e539603c6bbda6ac567a0dbbf04ae
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-04-09 09:48:18 +0000

    get rid of the stuff that is completely obsolete or has moved to examples
    git-svn-id: svn://localhost/mattie-subversion@1136 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/testing/parser-test.el
+++ trunk/testing/parser-test.el
@@ -32,55 +32,6 @@
 ;; parser function generation testing.
 ;;----------------------------------------------------------------------
 
-;; this seems to trigger correct pp-to-string behavior.
-(pp-to-string 'foo)
-
-;; test the single token/function case
-
-(parser-compile dump
-  (/token whitespace "[[:blank:]]+"))
-
-(parser-define 'test-parser
-  (parser-compile
-    (/token whitespace "[[:blank:]]+")))
-
-(functionp 'test-parser)
-
-;; simple test of the or relation.
-(parser-define 'test-parser
-  (parser-compile
-    (/token whitespace "[[:blank:]]+")
-    (/token word "[[:alpha:]]+")))
-
-;; test the and operator.
-(parser-compile dump
-  /and
-  (/token word "[[:alpha:]]+")
-  (/token whitespace "[[:blank:]]+"))
-
-;; test define
-(parser-define 'test-parser
-  (parser-compile dump
-    (define
-      (/token word "[[:alpha:]]+")
-      (/token whitespace "[[:blank:]]+"))
-    /and word whitespace))
-
-(parser-define 'test-parser
-  (parser-compile
-    /and
-    (/token word "[[:alpha:]]+")
-    (/token whitespace "[[:blank:]]+")))
-
-(parser-define 'test-parser
-  (parser-compile
-    /greedy
-    /and
-    (/token word "[[:alpha:]]+")
-    (/token whitespace "[[:blank:]]+")))
-
-parser foo bar baz||
-
 (parser-define 'test-parser
   (parser-compile
     dump
@@ -94,76 +45,6 @@ parser foo bar baz||
     ;; terminate /not
 
     word whitespace))
-
-;; not operator test.
-
-(parser-define 'test-parser
-  (parser-compile
-    dump
-    (define
-      (/token whitespace "[[:blank:]]+" null)
-      (/token word "[[:alpha:]]+")
-      (/token terminate "stop"))
-
-    /greedy
-    /and
-    terminate /not
-
-    word whitespace))
-
-start stop go less more
-
-(parser-define 'paludis-query
-  (parser-compile
-    dump
-    (define
-      (/token record-delim       "
-+" null)
-      (/token whitespace        "[[:blank:]]+"  null)
-      (/token pkg-name          "[^[:blank:]]+" parser-token-string)
-      (/token repo-name         "\\([^[:blank:]]+\\):" 1)
-
-      (/term pkg-version
-        /or
-        (/token pkg-ver-masked "\\\(\\([^[:blank:]]+\\)\\\)[^[:blank:]]+" 1)
-        (/token pkg-ver-stable "[^[:blank:]]+" parser-token-string)
-        (/token pkg-slot       "{:[[:digit:]]+}" null))
-      )
-
-    (/production package (/token package-record "\\\*" null) whitespace pkg-name)
-    (/production repository whitespace repo-name whitespace
-      (/greedy record-delim /not-skip pkg-version whitespace)) ))
-
-;;    (/production repository whitespace repo-name whitespace (/terminated (pkg-version whitespace) record-delim) ))
-
-(parser-trace-list paludis-trace
-  (package t)
-  (repository t))
-
-(parser-define 'paludis-query
-  (parser-compile
-    dump
-    (define
-      (/token whitespace        "[[:blank:]]+" null)
-      (/token pkg-name          "[^[:blank:]]+" parser-token-string)
-      (/token repo-name         "\\([^[:blank:]]+\\):" 1)
-      )
-
-    /and (/token package-record "\\\*" null) whitespace pkg-name)
-    ;; ^^ this list is the breaker, why ?
-    )
-
-;; failure test now
-(parser-compile dump
-  /bogus foo bar)
-
-;; check the default sequence
-(parser-semantic-dump
-  `(foo bar))
-
-;; test the closure case
-(parser-semantic-dump
-  `(/greedy foo bar))
 
 ;; test input effects
 (parser-semantic-dump
