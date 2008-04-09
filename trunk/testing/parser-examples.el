e9bfa53834b9c38235d3c3990fd71dc77d2b846a
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-04-09 09:41:28 +0000

    add the parser-examples
    git-svn-id: svn://localhost/mattie-subversion@1133 534069c3-6cd1-4c95-a44b-7f597ffc1621

---
+++ trunk/testing/parser-examples.el
@@ -0,0 +1,97 @@
+;;----------------------------------------------------------------------
+;; parser tests/examples
+;;----------------------------------------------------------------------
+
+(require 'parser) ;; evaluate this line to load the parser.
+
+;; error test, see what happens with bad input.
+(parser-compile
+  /bogus foo bar)
+
+;; test the single token/function case
+
+(parser-define 'test-parser
+  (parser-compile
+    (/token whitespace "[[:blank:]]+")))
+
+(functionp 'test-parser)
+
+;; simple test of the or relation.
+(parser-define 'test-parser
+  (parser-compile
+    ;; dump
+    (/token whitespace "[[:blank:]]+")
+    (/token word "[[:alpha:]]+")))
+
+  foo
+
+;; test the and operator.
+(parser-define 'test-parser
+  (parser-compile
+    ;; dump
+    /and
+    (/token word "[[:alpha:]]+")
+    (/token whitespace "[[:blank:]]+")))
+
+foo bar
+
+;; test define lists to pre-decl token/terms
+(parser-define 'test-parser
+  (parser-compile
+    ;; dump
+    (define
+      (/token word "[[:alpha:]]+")
+      (/token whitespace "[[:blank:]]+"))
+
+    /and word whitespace))
+
+;; test greed
+(parser-define 'test-parser
+  (parser-compile
+    ;; dump
+    /greedy
+    /and
+    (/token word "[[:alpha:]]+")
+    (/token whitespace "[[:blank:]]+")))
+
+parser foo bar baz||
+
+;; not operator test.
+
+(parser-define 'test-parser
+  (parser-compile
+    ;; dump
+    (define
+      (/token whitespace "[[:blank:]]+" null)
+      (/token word "[[:alpha:]]+")
+      (/token terminate "stop"))
+
+    /greedy
+    /and
+    terminate /not
+
+    word whitespace))
+
+start stop go less more
+
+;; attempt to parse a few lines from the paludis package manager reports.
+
+(parser-define 'paludis-query
+  (parser-compile
+;;    dump
+    (define
+      (/token record-delim       "$" null)
+      (/token whitespace        "[[:blank:]]+"  null)
+      (/token pkg-name          "\\([^[:blank:]]+\\)$" parser-token-string)
+      (/token repo-name         "\\([^[:blank:]]+\\):" 1)
+
+      (/term pkg-version
+        /or
+        (/token pkg-slot       "{:\\([[:digit:]]+\\)}" 1)
+        (/token pkg-ver-masked "(\\([^[:blank:]]+\\))[^[:blank:]]+" 1)
+        (/token pkg-ver-stable "[^[:blank:]]+" parser-token-string) ))
+
+    (/production package (/token package-record "\\\*" null) whitespace pkg-name)
+    (/production repository whitespace repo-name whitespace
+      (/greedy record-delim /not-skip pkg-version whitespace)) ))
+
