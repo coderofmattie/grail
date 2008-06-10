77fd7b92e148deab205decc4a443605258fc928e
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2008-06-10 16:22:33 +0000

    use correct update option when updating the source
    git-svn-id: svn://localhost/mattie-subversion@1170 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/dist/get-gnu-emacs.sh
+++ trunk/dist/get-gnu-emacs.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/sources/emacs co emacs
+if test -d emacs ; then
+  cd emacs && cvs update -dP
+else
+  cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/sources/emacs co emacs
+fi
+
