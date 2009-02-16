c8463b93aa61cc0caf6f8c0e88d230ae9ed340f2
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-02-16 22:49:32 +0000

    utilize the new grail-activate-with-recovery macro
    git-svn-id: svn://localhost/mattie-subversion@1318 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/local/styles/lisp.el
+++ trunk/local/styles/lisp.el
@@ -1,28 +1,40 @@
 ;;----------------------------------------------------------------------
 ;; lisp.el
 ;; Primary Author: Mike Mattie
-;; Copyright:
+;; Copyright: Mike Mattie 2009
 ;;----------------------------------------------------------------------
 
-;; fancy paren/delimited highlighting.
+;; mic-paren fancy paren/delimited highlighting. It is particularly valuable for reverse highlighting
+;;           regions.
 
-(unless
-  (robust-load-elisp
-    (require 'mic-paren)
+;; the built-in paren mode is a fallback until mic-paren can be activated.
 
-    (setq
-      paren-showing t
-      show-paren-style 'parenthesis
-      show-paren-delay 1
-      paren-sexp-mode 'match )
+(unless (grail-activate-with-recovery lisp mic-paren "http://www.emacswiki.org/cgi-bin/emacs/download/mic-paren.el"
+          (setq
+            paren-showing t
+            show-paren-style 'parenthesis
+            show-paren-delay 1
+            paren-sexp-mode 'match )
 
-    (paren-activate))
+          (paren-activate))
+
   ;; do the repair thingy
-  (grail-dup-error-to-scratch "the lisp style is hobbled by the unloadable mic-paren dependency"))
+  (grail-dup-error-to-scratch "the lisp style is hobbled by the broken mic-paren loading")
+
+  (grail-print-fn-to-scratch "activate-paren-mode-fallback" "activate the builtin paren mode a repair fallback")
+
+  (defun activate-paren-mode-fallback ()
+    (require 'paren)
+
+    (add-hook 'emacs-lisp-mode-hook
+      (lambda ()
+        (show-paren-mode))) ))
 
 ;; basic settings
 (setq
   lisp-indent-offset 2)
+
+;; if there are some long-term problems with mic-paren a backup solution is critical
 
 (defun swap-paren-keys ()
   "bind the parentheses to the brace keys, while the shifted
