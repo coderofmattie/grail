b458708a703be3328a376408ff98507bc375b159
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-03-01 23:30:37 +0000

    update the in-progress work of grail-recovery which is now limited to testing code that I will sort out later
    git-svn-id: svn://localhost/mattie-subversion@1347 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/experiments/grail-recovery.el
+++ trunk/experiments/grail-recovery.el
@@ -1,63 +1,83 @@
+
+
+
+;; (defun reto-hook-mode ( mode hook-fn )
+
+(mapc
+
+(get-url-with-last-modified
+  ;; can we build an update system in as well ?
+
+  )
+
+
+(defvar cfg-mod-icicle-install
+  '(("icicles.el"      . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles.el")
+    ("icicles-chg.el"  . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-chg.el")
+    ("icicles-cmd.el"  . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-cmd.el")
+    ("icicles-doc1.el" . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-doc1.el")
+    ("icicles-doc2.el" . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-doc2.el")
+    ("icicles-face.el" . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-face.el")
+    ("icicles-fn.el"   . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-fn.el")
+    ("icicles-mac.el"  . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-mac.el")
+    ("icicles-mcmd.el" . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-mcmd.el")
+    ("icicles-menu.el" . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-menu.el")
+    ("icicles-mode.el" . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-mode.el")
+    ("icicles-opt.el"  . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-opt.el")
+    ("icicles-var.el"  . "http://www.emacswiki.org/cgi-bin/wiki/download/icicles-var.el")
+    ("icomplete+.el"   . "http://www.emacswiki.org/cgi-bin/wiki/icomplete+.el/download/icomplete+.el")
+    ("hexrgb.el"       . "http://www.emacswiki.org/cgi-bin/wiki/icomplete+.el/download/hexrgb.el"))
+
+  "The icicles install list.")
+
+(setq foo 'cfg-mod-icicle-install)
+
+(put foo 'pkg-dir "icicles")
+
+(grail-install-package "icicles" 'cfg-mod-icicle-install)
+
+(grail-install-package "icicles" '(("foo" . "bar") ("baz" . "bing")))
+
+(grail-repair-by-installing 'foo '(("foo" . "bar")))
+(grail-repair-by-installing 'fubar '(("foo" . "bar") ("baz" . "bing")))
+
+;;  ("baz" . "bing")
+(grail-activate-with-recovery test foo
+  (("foo" . "bar"))
+
+  (setq foo "dead sexy")
+  (message "I am %s" foo))
+
+(type-of cfg-mod-icicle-install)
+
+(defun retro-major-mode-hook ( mode-match hook hook-fn )
+
+  ;; install the hook
+  (add-hook hook hook-fn)
+
+  (when grail-recovery-mode
+  ;; retro the hook to existing buffers
+  (mapc (lambda ( buf )
+          (with-current-buffer buf
+            (when (and (stringp major-mode) (string-match mode-match major-mode))
+              (funcall hook-fn)) )
+          ) (buffer-list)) ))
+
+;; retro-install-hook
+
+;; (buffer-list) ; get the buffer list
+
+(add-hook 'emacs-lisp-mode-hook
+      (lambda ()
+        (show-paren-mode))
+
+;; need to search for the buffer list and apply the hooks that are found.
+
 (grail-in-load-path-p "mic-parens") ;; should fail
 (grail-in-load-path-p "mic-paren")  ;; should succeed.
 
 (message "test the grail diagnostics %s"
   (format-signal-trap (diagnostic-load-elisp (error "%s" "bad things happened"))))
 
-(defmacro grail-initialize-with-recovery ( style package installer &rest initializer )
-  "grail-load-dep-with-recovery STYLE PKG-SYMBOL PKG-INSTALL INITIALIZER
+(grail-repair-dependency-fn 'mic-paren "http://www.emacswiki.org/cgi-bin/emacs/download/mic-paren.el")
 
-   attempt to load PKG-SYMBOL via require. If there are errors generate a repair function
-   that: installs the package and re-attempts loading and initialization.
-  "
-  (let
-    ((pkg-name        (symbol-name package))
-     (report-error    (lambda ( failure diagnostic repair-function )
-                        (format
-                          "grail: style %s is degraded from dependency %s failures %s\n; uncomment the function below to attempt repair\
-\n; (%s) " style pkg-name failure repair-function)))
-     (diagnostic   nil))
-
-    (when (catch 'abort ;; non-local exit for early termination from errors.
-
-      ;; try loading the package catching any diagnostic errors from signals
-      (when (setq diagnostic (diagnostic-load-elisp (require ',package)))
-
-        ;; create a recovery function.
-        (let
-          ((repair-fn-name (concat "repair-dependency-" pkg-name)))
-
-          ;; bind one of two recovery variations: a debugging setup if
-          ;; it is found, or an installer if the package is not found
-          ;; in load path.
-
-          (fset (intern repair-fn-name)
-            (if (grail-in-load-path-p pkg-name)
-              (progn
-                (funcall report-error "package loading" (format-signal-trap diagnostic) repair-fn-name)
-
-                (lambda ()
-                  (interactive)
-                  (grail-repair-by-debugging ',pkg-symbol) ))
-
-              (progn
-                (funcall report-error "package installation"
-                  "cannot find package in load-path" repair-fn-name)
-
-                (lambda ()
-
-                  )) )))
-
-        (throw 'abort t))
-
-      (unless (robust-load-elisp ,initializer)
-          (grail-dup-error-to-scratch "initialization failed")
-          (throw 'abort nil))
-
-            (grail-dup-error-to-scratch
-              (format "grail: style %s is degraded from failure to load %s\n; uncomment the function below to attempt repair\
-\n; (%s) " style pkg-name repair-fn-name)) )
-
-          (throw 'abort nil) )
-
-        t))))
