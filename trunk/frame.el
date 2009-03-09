e9de6c32f88b59c56cdaeb2264fe424a6aa84278
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-03-09 14:45:14 +0000

    lock in the size of the frame to 85x28 in characters to prevent the problems with the frame being to large on small displays
    git-svn-id: svn://localhost/mattie-subversion@1421 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/frame.el
+++ trunk/frame.el
@@ -8,5 +8,7 @@
 (setq default-frame-alist
   (append '((cursor-color . "yellow")
             (mouse-color  . "red")
-            (cursor-type  . "hollow"))
+            (cursor-type  . "hollow")
+            (width        . 85)
+            (height       . 28))
     default-frame-alist))
