2f725383af159fc16f6e626039f3f2b5cebc8259
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-03-09 20:05:19 +0000

    get rid of the crud in gui.el
    git-svn-id: svn://localhost/mattie-subversion@1429 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/gui.el
+++ trunk/gui.el
@@ -5,10 +5,5 @@
 
 (use-grail-groups "complete")
 
-;; colors for the cursor and the background. Kept in visual.el since
-;; the colors need to be harmonious.
-
 (setq use-dialog-box nil)                     ;; kill the dialogs before they strike.
-
-(blink-cursor-mode -1)
 
