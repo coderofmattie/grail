82f5a2f1db7b9deb0ccbf7923cbcb6e058fc0cf3
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-03-09 19:17:33 +0000

    move most of the color and interactive stuff into frame.el, leave the clearly gui only stuff in gui.el
    git-svn-id: svn://localhost/mattie-subversion@1427 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/frame.el
+++ trunk/frame.el
@@ -3,6 +3,8 @@
 ;; written by Mike Mattie
 ;;----------------------------------------------------------------------
 
+(blink-cursor-mode -1)
+
 ;; parameters that need to be setup before frames are created.
 
 (setq default-frame-alist
@@ -12,3 +14,40 @@
             (width        . 85)
             (height       . 28))
     default-frame-alist))
+
+(grail-set-faces
+  ;; the default face
+
+  (default
+    (background "grey5")
+    (foreground "grey70")
+    (inverse-video nil)
+    (box nil)
+    (strike-through nil)
+    (overline nil)
+    (underline nil)
+    (slant 'normal)
+    (weight 'normal)
+    (height 125)
+    (width 'normal)
+    (family "DejaVuLisp"))
+
+  ;; comments are set off-tempature to distingiush them better.
+  ;; orange was chosen as a red that wasn't harsh.
+  (font-lock-comment-face (foreground "orange3"))
+
+  ;; language syntax is the darkest shade of blue
+  (font-lock-keyword-face (foreground "DeepSkyBlue4"))
+
+  ;; grammar is the lightest shade of blue
+  (font-lock-builtin-face (foreground "SkyBlue3"))
+
+  ;; this should be for any form of literal value in code medium contrast.
+  (font-lock-string-face  (foreground "grey50"))
+  (font-lock-constant-face (foreground "grey50"))
+
+  ;; decl is dark green
+  (font-lock-type-face (foreground "green4"))
+  (font-lock-function-name-face (foreground "aquamarine4"))
+  (font-lock-variable-name-face (foreground "aquamarine3"))
+  )
--- trunk/gui.el
+++ trunk/gui.el
@@ -10,47 +10,5 @@
 
 (setq use-dialog-box nil)                     ;; kill the dialogs before they strike.
 
-;; (add-to-list 'default-frame-alist '(cursor-color  . "yellow"))
-
-;; DejaVuLisp was modified from DejaVuMono from this link.
-;; http://dejavu.sourceforge.net/wiki/index.php/Main_Page
-
-(grail-set-faces
-  ;; the default face
-
-  (default
-    (background "grey5")
-    (foreground "grey70")
-    (inverse-video nil)
-    (box nil)
-    (strike-through nil)
-    (overline nil)
-    (underline nil)
-    (slant 'normal)
-    (weight 'normal)
-    (height 125)
-    (width 'normal)
-    (family "DejaVuLisp"))
-
-  ;; comments are set off-tempature to distingiush them better.
-  ;; orange was chosen as a red that wasn't harsh.
-  (font-lock-comment-face (foreground "orange3"))
-
-  ;; language syntax is the darkest shade of blue
-  (font-lock-keyword-face (foreground "DeepSkyBlue4"))
-
-  ;; grammar is the lightest shade of blue
-  (font-lock-builtin-face (foreground "SkyBlue3"))
-
-  ;; this should be for any form of literal value in code medium contrast.
-  (font-lock-string-face  (foreground "grey50"))
-  (font-lock-constant-face (foreground "grey50"))
-
-  ;; decl is dark green
-  (font-lock-type-face (foreground "green4"))
-  (font-lock-function-name-face (foreground "aquamarine4"))
-  (font-lock-variable-name-face (foreground "aquamarine3"))
-  )
-
 (blink-cursor-mode -1)
 
