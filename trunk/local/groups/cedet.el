063d32813684bfa631811d6af31d61f469811823
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-03-11 19:33:51 +0000

    throw cedet overboard. The installation is truly complex, beyond the scope of grail, and the actuall configuration of it is mind-boggling
    git-svn-id: svn://localhost/mattie-subversion@1447 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/local/groups/cedet.el
+++
@@ -1,9 +0,0 @@
-;;----------------------------------------------------------------------
-;; cedet.el
-;;----------------------------------------------------------------------
-
-(load-elisp-if-exists (concat grail-dist-elisp "cedet/common/cedet.el"))
-
-(semantic-load-enable-minimum-features)
-
-(semantic-load-enable-code-helpers)
