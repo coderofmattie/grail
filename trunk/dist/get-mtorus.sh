80a65c9b0367ea907cfa308a00f5f5f731930f98
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-03-12 20:27:34 +0000

    get rid of the mtorus downloader since I don't use mtorus anymore
    git-svn-id: svn://localhost/mattie-subversion@1474 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/dist/get-mtorus.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# http://www.emacswiki.org/cgi-bin/wiki?MTorus
-
-# http://mtorus.berlios.de/
-
-cd elisp/ && cvs -z3 -d:pserver:anonymous@cvs.mtorus.berlios.de:/cvsroot/mtorus co mtorus
