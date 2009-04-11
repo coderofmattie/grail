44eadcd58de38744eb8cb803c7bfd5714dc1813c
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-04-11 00:31:48 +0000

    remove get-overlays and yasnippet which are now handled by grail
    git-svn-id: svn://localhost/mattie-subversion@1527 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/dist/get-overlays.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-dir="elisp/"
-url="http://www.dr-qubit.org/download.php?file=predictive/auto-overlays-0.9.1.tar.gz&counter=auto-overlays.tar.gz"
-test -d $dir || mkdir $dir
-cd $dir && wget -O - $url | tar xzf -
