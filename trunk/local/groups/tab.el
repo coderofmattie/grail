991c4d16293f8caff6753f82fc0e2725faf6ca37
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2009-04-13 05:51:49 +0000

    template group   * implement a new template system based on yasnippet.
    xml-code.el
      * delete xml-code which is obsoleted by the yasnippet based system.
    
    tab.el
      * modify tab.el to use the template system.
    
    
    
    git-svn-id: svn://localhost/mattie-subversion@1540 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/local/groups/tab.el
+++ trunk/local/groups/tab.el
@@ -3,18 +3,39 @@
 ;;----------------------------------------------------------------------
 (require 'dwim-tab)
 
+(defun template-localize-tab ( &rest complete )
+  (apply 'dwim-tab-localize
+    (append
+      (when (templates-enabled-p)
+        (dwim-tab-local-context 'template-next-next-field)
+        (list 'template-expand))
+      complete)))
+
+;; emacs builtin modes
+
 (add-hook 'emacs-lisp-mode-hook
   (lambda ()
-    (dwim-tab-localize 'lisp-complete-symbol)))
-
-(add-hook 'nxml-mode-hook
-  (lambda ()
-    (dwim-tab-localize 'nxml-complete)))
+    (template-localize-tab 'lisp-complete-symbol))
+  t)
 
 (add-hook 'eshell-mode-hook
   (lambda ()
     ;; ensure that none of my custom keybindings are affected.
     (dwim-tab-localize 'pcomplete-expand-and-complete) ))
 
+;; setup for modes that are not automatically loaded.
+(eval-after-load 'scheme-mode
+  ;; there is no scheme completion that I know of ... fsck!
+  '(add-hook 'scheme-mode-hook 'template-localize-tab t))
+
+(eval-after-load 'nxml-mode
+  '(add-hook 'nxml-mode-hook
+     (lambda ()
+       (template-localize-tab 'nxml-complete))
+     t))
+
 (eval-after-load 'cperl-mode
-  '(add-hook 'cperl-mode-hook 'dwim-tab-localize))
+  '(add-hook 'cperl-mode-hook 'template-localize-tab t))
+
+
+
--- trunk/local/groups/template.el
+++ trunk/local/groups/template.el
@@ -6,27 +6,57 @@
   (grail-define-installer "yasnippet" "tar:bz2"
     "http://yasnippet.googlecode.com/files/yasnippet-0.5.10.tar.bz2") "the yasnippet installer")
 
+(defvar templates-enabled nil)
+(defvar templates-loaded nil "a list of the languages with templates loaded")
+
+(defun templates-enabled-p ()
+  "templates-enabled-p
+
+   return non-nil when templates are enabled.
+  "
+  (when templates-enabled t))
+
 (grail-activate-with-recovery "template" yasnippet template-group-yasnippet-installer
   ;; don't automatically activate.
   (setq yas/dont-activate t)
 
-  (defun in-yasnippet-reference ()
-    (interactive)
+  (defvar template-group-yasnippet-tree
+    (grail-sanitize-path (concat grail-elisp-root "templates/yasnippet/"))
+    "the yasnippet tree path relative to grail-elisp-root")
+
+  ;; these have the generic template reference so I can switch or mix
+  ;; template systems without going on a grep marathon.
+
+  (defun template-in-reference ()
     (when (mode-overlay-at-point-p 'yas/snippet-reference) t))
 
-  (defun dwim-tab-yas-next-field ()
-    (when (in-yasnippet-reference)
+  (defun template-next-next-field ()
+    (interactive)
+    (when (template-in-reference)
       (yas/next-field-group)
       t))
 
-  (defun dwim-tab-yas-expand ()
-    (interactive)
-    (when (equal 'expanded (yas/expand)) t))
-  )
+  (defun templates-load-for-language ( language )
+    (let
+      ((templates (concat template-group-yasnippet-tree language)))
 
-;; for each mode we can do a load ....
-;;'yas/minor-mode-auto-on
-;;  (yas/load-directory (grail-garuntee-dir-path (grail-sanitize-path (concat grail-elisp-root "templates/yasnippet"))))
+      (when (file-accessible-directory-p templates)
+        (yas/load-directory templates)
+        (add-to-list 'templates-loaded language)) ))
+
+  (defun template-expand ()
+    (interactive)
+    (when (equal 'expanded (yas/expand)) (throw 'terminate-complete t))
+    nil)
+
+  ;; signal that a template system is activated
+  (setq templates-enabled t)
+
+  ;; always load the lisp templates.
+  (templates-load-for-language "lisp")
+
+  (eval-after-load 'cperl-mode
+    '(templates-load-for-language "perl")))
 
 
 
