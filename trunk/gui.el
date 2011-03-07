9fc75c13bacc6a279a9933890c1cf1d0a5341fec
version-control <version-control@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2011-03-07 04:30:29 +0000

    kill dead code
    git-svn-id: svn://localhost/mattie-subversion@1579 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/gui.el
+++ trunk/gui.el
@@ -3,8 +3,6 @@
 ;; Primary Author: Mike Mattie
 ;;----------------------------------------------------------------------
 
-;; (use-grail-groups "complete")
-
 (setq use-dialog-box nil)                     ;; kill the dialogs before they strike.
 
 (eval-after-load 'flyspell
