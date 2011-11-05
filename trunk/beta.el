626a818765ae8784aad99c3e434204aefedaa3cb
mattie <mattie@534069c3-6cd1-4c95-a44b-7f597ffc1621>
2011-11-05 15:22:09 +0000

    finally add support for win32 to the windows maximize function
    git-svn-id: svn://localhost/mattie-subversion@1598 534069c3-6cd1-4c95-a44b-7f597ffc1621

--- trunk/beta.el
+++ trunk/beta.el
@@ -128,10 +128,12 @@
           '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
 
         (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-          '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-        ))
-      ((message "window system %s is not supported by maximize" (symbol-name (window-system))))
-    ))
+          '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0)) ))
+
+    ((eq 'w32 (window-system))
+      (w32-send-sys-command 61488))
+
+    ((message "window system %s is not supported by maximize" (symbol-name (window-system)))) ))
 
 (defun fullscreen ()
   (interactive)
