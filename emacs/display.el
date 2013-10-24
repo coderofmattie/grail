e974ae0635a78d6aedbd98fddcf85e135f226425
Mike Mattie <codermattie@gmail.com>
2013-10-23 18:05:24 -0700

    emacs started bombing out when I was setting the ediff faces before it loaded. fix that

--- emacs/display.el
+++ emacs/display.el
@@ -42,7 +42,6 @@
     (width 'normal)
     (family (select-best-font-family)))
 
-
   ;; comments are set off-tempature to distingiush them better.
   ;; orange was chosen as a red that wasn't harsh.
   (font-lock-comment-face (foreground "orange3"))
@@ -62,19 +61,7 @@
   ;; decl is dark green
   (font-lock-type-face (foreground "green4"))
   (font-lock-function-name-face (foreground "aquamarine4"))
-  (font-lock-variable-name-face (foreground "aquamarine3"))
-
-  (ediff-current-diff-face-A (background "grey13") (foreground "dark goldenrod"))
-  (ediff-current-diff-face-B (background "grey13") (foreground "dark khaki"))
-  (ediff-current-diff-face-C (background "grey13") (foreground "olive drab"))
-
-  (ediff-even-diff-face-A (background "grey12") (foreground "light slate grey"))
-  (ediff-even-diff-face-B (background "grey12") (foreground "light slate grey"))
-  (ediff-even-diff-face-C (background "grey12") (foreground "light slate grey"))
-
-  (ediff-fine-diff-face-A  (underline "orange2"))
-  (ediff-fine-diff-face-B  (underline "orange2"))
-  (ediff-fine-diff-face-C  (underline "orange2")) )
+  (font-lock-variable-name-face (foreground "aquamarine3")) )
 
 (set-face-attribute 'default nil
   :font (concat (select-best-font-family) ":spacing=m"))
@@ -96,3 +83,16 @@
 
      (set-face-background 'cperl-nonoverridable-face "grey5")
      (set-face-foreground 'cperl-nonoverridable-face "DeepSkyBlue4") ))
+
+(eval-after-load 'ediff
+  '(progn
+     (ediff-current-diff-face-A (background "grey13") (foreground "dark goldenrod"))
+     (ediff-current-diff-face-B (background "grey13") (foreground "dark khaki"))
+     (ediff-current-diff-face-C (background "grey13") (foreground "olive drab"))
+
+     (ediff-even-diff-face-A (background "grey12") (foreground "light slate grey"))
+     (ediff-even-diff-face-B (background "grey12") (foreground "light slate grey"))
+     (ediff-even-diff-face-C (background "grey12") (foreground "light slate grey"))
+
+     (ediff-fine-diff-face-A  (underline "orange2"))
+     (ediff-fine-diff-face-B  (underline "orange2")) ))
