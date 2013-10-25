45bcb9aea8e219b20eb0c27a7a11f8905c5595e3
Mike Mattie <Michael.Mattie@adp.com>
2013-10-25 00:35:54 -0700

    band-aid the cperl double init problem for now

--- emacs/programming.el
+++ emacs/programming.el
@@ -327,28 +327,29 @@
 ;;----------------------------------------------------------------------
 ;; perl5
 ;;----------------------------------------------------------------------
-(eval-after-load 'cperl-mode
-  '(progn
-     (setq
-       cperl-indent-parens-as-block t
-
-       cperl-indent-level 2
-       cperl-continued-statement-offset 2
-
-       cperl-close-paren-offset -2
-
-       cperl-indent-subs-specially nil
-
-       cperl-highlight-variables-indiscriminately t
-
-       cperl-electric-parens t)) )
-
 ;; make cperl the default in all cases.
 (mapc
   (lambda (pair)
     (if (eq (cdr pair) 'perl-mode)
       (setcdr pair 'cperl-mode)))
      (append auto-mode-alist interpreter-mode-alist))
+
+(require 'cperl-mode)
+
+(progn
+  (setq
+    cperl-indent-parens-as-block t
+
+    cperl-indent-level 2
+    cperl-continued-statement-offset 2
+
+    cperl-close-paren-offset -2
+
+    cperl-indent-subs-specially nil
+
+    cperl-highlight-variables-indiscriminately t
+
+    cperl-electric-parens t))
 
 (defconst perl-function-regex "sub")
 
@@ -358,14 +359,21 @@
 
 (add-hook 'cperl-mode-hook
   (lambda ()
-    (set-face-foreground cperl-pod-face "orange3")
+    (unless (boundp 'cperl-mode-configured)
+      (set-face-foreground cperl-pod-face "orange3")
 
-    (configure-for-navigation 'forward-word 'backward-word)
-    (configure-for-programming 'perl-list-fn-signatures "perl-mode")
+      (configure-for-navigation 'forward-word 'backward-word)
+      (configure-for-programming 'perl-list-fn-signatures "perl-mode")
 
-    (local-set-key (kbd "C-h f") 'cperl-perldoc-at-point)
+      (local-set-key (kbd "C-h f") 'cperl-perldoc-at-point)
 
-    (turn-on-dwim-tab 'cperl-indent-command) )
+      (turn-on-dwim-tab 'cperl-indent-command)
+
+      ;; cperl-mode-hook gets called twice. somewhere in cperl-mode
+      ;; emacs is setting cperl-mode-hook as a delayed hook execution
+      ;; and running all the hooks twice. would like to figure that
+      ;; out but this will work for now.
+      (set (make-variable-buffer-local 'cperl-mode-configured) t) ))
   t)
 
 ;;----------------------------------------------------------------------
