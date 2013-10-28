e93f00f3c0c5bd710bcfca9b3e4e6c031a7e6a14
Mike Mattie <codermattie@gmail.com>
2013-10-27 21:50:27 -0700

    I now have a working slime setup

--- emacs/local/profiles/slime.el
+++ emacs/local/profiles/slime.el
@@ -5,8 +5,8 @@
 ;;----------------------------------------------------------------------
 
 (grail-load 'slime     (grail-define-installer "slime"
-                         "pkg"
-                         'slime))
+                         "cvs"
+                         ":pserver:anonymous:anonymous@common-lisp.net:/project/slime/cvsroot" ))
 
 ;; (grail-load 'slime-repl  (grail-define-installer "slime-repl"
 ;;                           "pkg"
@@ -15,25 +15,24 @@
 ;;----------------------------------------------------------------------
 ;; SLIME
 ;;----------------------------------------------------------------------
-;; (require 'slime)
+(require 'slime)
 
-;; (setq
-;;   slime-net-coding-system 'utf-8-unix
+(setq
+  slime-net-coding-system 'utf-8-unix
 
-;;   inferior-lisp-program "sbcl")
+  inferior-lisp-program "sbcl"
+
+  slime-words-of-encouragement '("The name is Bond. James Bond."
+                                  "These are your father's parentheses. Elegant weapons from a more civilized age."
+                                  "We were on the edge of the desert when the Emacs took hold."
+                                  "Mine says: Desert Eagle ... .50"))
 
 ;; (add-hook 'lisp-mode-hook (lambda ()
 ;;                             (slime-mode t)))
 
-;; (add-hook 'inferior-lisp-mode-hook
-;;   (lambda () (inferior-slime-mode t)))
-
+(add-hook 'inferior-lisp-mode-hook
+  (lambda () (inferior-slime-mode t)))
 
 ;; (slime-setup '(slime-repl))
 
-;; (setq
-;;   slime-words-of-encouragement '("The name is Bond. James Bond."
-;;                                  "These are your father's parentheses. Elegant weapons from a more civilized age."
-;;                                  "We were on the edge of the desert when the Emacs took hold."
-;;                                  "Mine says: Desert Eagle ... .50"))
 
