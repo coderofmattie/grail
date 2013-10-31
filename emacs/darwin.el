2e63b072ceeaaeb900ef9ad8617c50c468476f09
Mike Mattie <Michael.Mattie@adp.com>
2013-10-30 20:05:27 -0700

    try and not get raped by darwin yet again. construct exec-path from /etc/paths

--- emacs/darwin.el
+++ emacs/darwin.el
@@ -12,6 +12,30 @@
 
 (mask-grail-profiles "slime" "spell")
 
+(defun osx-merge-etc-paths-file ()
+  (append
+    (split-string (with-temp-buffer
+                    (insert-file-contents-literally "/etc/paths")
+
+                    (buffer-substring-no-properties
+                      (progn
+                        (beginning-of-buffer)
+                        (point))
+                      (progn
+                        (end-of-buffer)
+                        (point)) )) )
+    exec-path))
+
+;; unfuck osx which has no sane way to handle environment variables
+;; or paths. only launchd.conf in /etc which sets variables globally
+;; is honored.
+;;
+;; paths which works better is not honored except in the shell.
+
+(setq exec-path (osx-merge-etc-paths-file))
+
+
+
 
 
 
