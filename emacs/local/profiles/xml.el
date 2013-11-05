a01813bbc1d2415ca5ee3e00f6971d0a15a0ea45
Mike Mattie <Michael.Mattie@adp.com>
2013-11-04 23:29:46 -0800

    move the xml stuff into a profile

--- emacs/local/profiles/xml.el
+++ emacs/local/profiles/xml.el
@@ -1,6 +1,19 @@
 ;;----------------------------------------------------------------------
 ;;                    xml/html
 ;;----------------------------------------------------------------------
+(require 'nxml-mode)
+
+(setq auto-mode-alist (append '( ("\\.html$"    . nxml-mode)
+                                 ("\\.xhtml$"   . nxml-mode)
+                                 ("\\.xml$"     . nxml-mode)
+                                 ) auto-mode-alist ))
+
+(add-hook 'nxml-mode-hook
+  (lambda ()
+    (dwim-tab-localize-context 'nxml-complete)
+    (turn-on-dwim-tab 'nxml-indent-line)
+    (configure-for-buffer-ring "nxml-mode"))
+  t)
 
 
 
