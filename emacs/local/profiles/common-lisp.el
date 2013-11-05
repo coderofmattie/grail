2b462aa10ad5e6d9bf369413e35b186cd73c7b6b
Mike Mattie <codermattie@gmail.com>
2013-11-04 20:40:33 -0800

    breakout common lisp into a profile

---
+++ emacs/local/profiles/common-lisp.el
@@ -0,0 +1,22 @@
+;;----------------------------------------------------------------------
+;; common lisp
+;;----------------------------------------------------------------------
+(defvar cl-function-decl ".*(defun.*")
+
+(defun cl-list-functions ()
+  (interactive)
+  (occur cl-function-decl))
+
+(require 'lisp-mode)
+
+(setq lisp-mode-hook nil)
+
+(add-hook 'lisp-mode-hook
+  (lambda ()
+    (configure-for-programming 'cl-list-functions "lisp-mode")
+
+    (lisp-smart-parens-editing)
+
+    (turn-on-dwim-tab 'common-lisp-indent-function) )
+  t)
+
