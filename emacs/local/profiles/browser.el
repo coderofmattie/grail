96ee9c2b043b62c29cea14314e4c2ec7aa6d8715
Mike Mattie <codermattie@gmail.com>
2013-11-04 21:02:06 -0800

    add w3 but that did not help with the hyperspec

--- emacs/local/profiles/browser.el
+++ emacs/local/profiles/browser.el
@@ -8,5 +8,8 @@
                   "file"
                   "http://bzr.savannah.gnu.org/lh/emacs/trunk/download/head:/eww.el-20130610114603-80ap3gwnw4x4m5ix-1/eww.el"))
 
+(grail-load 'w3 (grail-define-installer "w3"
+                  "pkg"
+                  'w3))
 
 
