a8c13610895bd77089ba822e75a1f68738bf3935
Mike Mattie <Michael.Mattie@adp.com>
2013-11-05 00:40:39 -0800

    fix the ugly faces used when a pair is inserted

--- emacs/local/profiles/code-editing.el
+++ emacs/local/profiles/code-editing.el
@@ -7,7 +7,9 @@
                             'smartparens))
 
 (strip-minor-mode-keymap 'smartparens-mode)
-
+(set-face-background 'sp-pair-overlay-face "grey10")
+(set-face-background 'sp-wrap-overlay-face "grey10")
+(set-face-background 'sp-wrap-tag-overlay-face "grey10")
 
 
 
