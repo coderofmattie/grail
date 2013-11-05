1fd0ac24f016e74c32412f38241047fee3838610
Mike Mattie <Michael.Mattie@adp.com>
2013-11-05 00:40:05 -0800

    get a popup browse going

--- emacs/local/profiles/browser.el
+++ emacs/local/profiles/browser.el
@@ -4,6 +4,24 @@
 ;; browser support required for documentation usually
 ;;----------------------------------------------------------------------
 
+(defun popup-browse-html ( url &rest args)
+  (let
+    ((doc-buffer-name (generate-new-buffer-name "browser")))
+
+    (pop-to-buffer doc-buffer-name)
+    (eww url) ))
+
+(defun set-popup-browse-html-local ()
+  (make-variable-buffer-local 'browse-url-browser-function)
+  (setq browse-url-browser-function 'popup-browse-html))
+
+    ;; (with-current-buffer doc-buffer-name
+    ;;   (make-variable-buffer-local 'kill-buffer-hook)
+    ;;   (add-hook 'kill-buffer-hook
+    ;;     (lambda ()
+    ;;       (rid-window))
+    ;;     t)
+
 
 
 
