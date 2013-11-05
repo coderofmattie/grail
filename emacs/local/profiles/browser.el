8ad7001385073aaed4473520b9376c3a8b2fb2cc
Mike Mattie <Michael.Mattie@adp.com>
2013-11-04 23:27:53 -0800

    remove both browsers as they are broken. w3 is broken. eww needs a current emacs to run on

--- emacs/local/profiles/browser.el
+++ emacs/local/profiles/browser.el
@@ -4,12 +4,6 @@
 ;; browser support required for documentation usually
 ;;----------------------------------------------------------------------
 
-(grail-load 'eww (grail-define-installer "eww"
-                  "file"
-                  "http://bzr.savannah.gnu.org/lh/emacs/trunk/download/head:/eww.el-20130610114603-80ap3gwnw4x4m5ix-1/eww.el"))
 
-(grail-load 'w3 (grail-define-installer "w3"
-                  "pkg"
-                  'w3))
 
 
