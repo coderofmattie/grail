217561546a447d06e980c4670a18fa86c6ab591b
Mike Mattie <Michael.Mattie@adp.com>
2013-11-05 00:41:34 -0800

    finally after a incredible detour get hyperspec doc lookup going

--- emacs/local/profiles/slime.el
+++ emacs/local/profiles/slime.el
@@ -27,11 +27,15 @@
   slime-words-of-encouragement '("The name is Bond. James Bond."
                                   "These are your father's parentheses. Elegant weapons from a more civilized age."
                                   "We were on the edge of the desert when the Emacs took hold."
-                                  "Mine says: Desert Eagle ... .50"))
+                                  "Mine says: Desert Eagle ... .50")
+  common-lisp-hyperspec-root (concat "file://" hyperspec-dir "/"))
 
 (add-hook 'slime-connected-hook
   (lambda ()
     (lisp-smart-parens-editing)
+
+    (set-popup-browse-html-local)
+    (configure-for-docs 'slime-hyperspec-lookup)
 
     (configure-for-buffer-ring "lisp-mode")
 
@@ -41,6 +45,9 @@
 (add-hook 'lisp-mode-hook
   (lambda ()
     (slime-mode t)
+
+    (set-popup-browse-html-local)
+    (configure-for-docs 'slime-hyperspec-lookup)
 
     (dwim-tab-localize-context 'slime-complete-symbol)
 
