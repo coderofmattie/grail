c7d1b4752cd610b4e317f3089942cf510a6394c2
Mike Mattie <codermattie@gmail.com>
2013-11-08 08:55:54 -0800

    move the socks stuff out of darwin and into rouge

--- emacs/darwin.el
+++ emacs/darwin.el
@@ -34,17 +34,6 @@
 
 (setq exec-path (osx-merge-etc-paths-file))
 
-(require 'erc)
-(require 'socks)
-
-(add-hook 'erc-mode-hook
-  (lambda ()
-    (setq socks-noproxy '("localhost"))
-    (setq erc-server-connect-function 'socks-open-network-stream)
-
-    (setq socks-server (list "tor proxy" "localhost" 9050 5)) )
-  t)
-
 
 
 
