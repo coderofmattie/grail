72d4d88242bd9be65ce62c287c82e47f9b2ea2c1
Mike Mattie <codermattie@gmail.com>
2013-11-13 12:47:09 -0800

    get tcp server mode running and lock down the server file so it will actually work.

--- emacs/grail.el
+++ emacs/grail.el
@@ -280,6 +280,16 @@
         "grail.el replacement for daemonp function."
         nil))
 
+    (when (daemonp)
+      (let
+        ((server-dir (file-name-directory (getenv "EMACS_SERVER_FILE"))))
+
+        (when (file-accessible-directory-p server-dir)
+          (setq
+            server-socket-dir server-dir
+            server-auth-dir server-dir
+            server-use-tcp t)) ))
+
     ;;----------------------------------------------------------------------
     ;; load the configuration based on mode.
     ;;----------------------------------------------------------------------
