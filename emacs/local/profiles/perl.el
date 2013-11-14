84126b2348433d854be086441b9ff889144937b3
Mike Mattie <codermattie@gmail.com>
2013-11-13 20:47:55 -0800

    the old way of detecting that cperl ran twice is not working anymore. just let it run again this time

--- emacs/local/profiles/perl.el
+++ emacs/local/profiles/perl.el
@@ -34,22 +34,15 @@
 
 (add-hook 'cperl-mode-hook
   (lambda ()
-    (unless (boundp 'cperl-mode-configured)
-      (set-face-foreground cperl-pod-face "orange3")
+    (set-face-foreground cperl-pod-face "orange3")
 
-      (configure-for-navigation 'forward-word 'backward-word)
-      (configure-for-programming 'perl-list-fn-signatures "perl-mode")
+    (configure-for-navigation 'forward-word 'backward-word)
+    (configure-for-programming 'perl-list-fn-signatures "perl-mode")
 
-      (local-set-key (kbd "C-h f") 'cperl-perldoc-at-point)
+    (local-set-key (kbd "C-h f") 'cperl-perldoc-at-point)
 
-      (turn-on-dwim-tab 'cperl-indent-command)
+    (turn-on-dwim-tab 'cperl-indent-command)
 
-      ;; cperl-mode-hook gets called twice. somewhere in cperl-mode
-      ;; emacs is setting cperl-mode-hook as a delayed hook execution
-      ;; and running all the hooks twice. would like to figure that
-      ;; out but this will work for now.
-      (set (make-variable-buffer-local 'cperl-mode-configured) t)
-
-      (procedural-smart-parens-editing)
-      (setq sp-escape-char "\\") ))
+    (procedural-smart-parens-editing)
+    (setq sp-escape-char "\\"))
   t)
