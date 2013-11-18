bb72ddbed3f909fd3b7ee8e4ecf929bde2b1561b
Mike Mattie <codermattie@gmail.com>
2013-11-18 11:34:41 -0800

    add some perforce support

---
+++ emacs/local/elisp/perforce-utilities.el
@@ -0,0 +1,67 @@
+;;----------------------------------------------------------------------
+;; dealing with perforce issues.
+;;----------------------------------------------------------------------
+(require 'rw-utilities)
+(require 'working-copy)
+
+(defvar perforce-trees nil)
+
+(defun perforce-in-use-p ()
+  (if (perfoce-trees)
+    t
+    nil))
+
+(defun pforce-add-tree ( tree )
+  (setq perforce-trees (cons tree perforce-trees)) )
+
+(defun pforce-is-subdir-of ( root-path child-path )
+  (catch 'done
+    (let
+      ((root-dirs  (split-string root-path  "[/]"))
+       (child-dirs (split-string child-path "[/]"))
+       (is-child nil))
+
+      (while t
+        (let
+          ((next-root (car root-dirs))
+           (next-child (car child-dirs)))
+
+          (setq root-dirs (cdr root-dirs))
+          (setq child-dirs (cdr child-dirs))
+
+          (unless (and next-root next-child)
+            (throw 'done nil))
+
+          (unless (string-equal next-root next-child)
+            (throw 'done nil))
+
+          (when (eq nil root-dirs)
+            (setq is-child t)
+            (throw 'done t)) )) )
+    is-child))
+
+(defun perforce-is-perforce-tree ( path )
+  (let
+    ((query-path (if (file-directory-p path) path (file-name-directory path))))
+
+    (catch 'is-pforce
+      (mapc (lambda ( perforce-tree )
+              (when (pforce-is-subdir-of perforce-tree query-path)
+                (throw 'is-pforce t)))
+        pforce-trees)
+
+      nil)))
+
+(defun perforce-convert-to-working-copy-hook ()
+  (interactive)
+
+  (when (and (perforce-is-perforce-tree buffer-file-name) (not (wc-working-copy-buffer-p)))
+    (when (eq 't (y-or-n-p "P4 locked file convert -> working copy? "))
+      (wc-init-for-buffer)
+      (rw-make-path-writable buffer-file-name)
+      (message "converted this buffer from perforce -> working copy") )))
+
+(defun perforce-activate-with-working-copy ()
+  (add-hook 'before-save-hook 'perforce-convert-to-working-copy-hook))
+
+(provide 'perforce-utilities)
--- emacs/local/elisp/working-copy.el
+++ emacs/local/elisp/working-copy.el
@@ -15,7 +15,9 @@
 (defun wc-write-to-file ( path )
   (save-excursion
     (let
-      ((end-of-file (end-of-buffer)))
+      ((end-of-file (progn
+                      (end-of-buffer)
+                      (point))))
 
       (beginning-of-buffer)
       (write-region (point) end-of-file path) )))
@@ -24,9 +26,11 @@
   (wc-write-to-file (wc-ancestor-file)))
 
 (defun wc-update-local-file ()
+  (interactive)
   (wc-write-to-file (wc-local-file)))
 
 (defun wc-update-upstream-file ()
+  (interactive)
   (wc-write-to-file (wc-upstream-file)))
 
 (defun wc-working-copy-file-p ( path )
@@ -35,8 +39,57 @@
 (defun wc-working-copy-buffer-p ()
   (file-readable-p (wc-local-file)))
 
-(defun wc-init-for-buffer ()
+(defun wc-command-buffer ()
+  (get-buffer-create "*working-copy-rcs*"))
+
+(defun wc-run-rcs-command ( command args )
+  (catch 'abort
+    (let
+      ((rcs-buffer (wc-command-buffer))
+       (check-in-dir (file-name-directory (wc-local-file))))
+
+      (unless (and check-in-dir (file-directory-p check-in-dir))
+        (message "wc RCS abort! directory %s does not exist!")
+        (throw 'abort))
+
+      (let
+        ((rcs-storage (concat check-in-dir "/RCS")))
+
+        (unless (file-directory-p rcs-storage)
+          (make-directory rcs-storage)) )
+
+      (with-current-buffer rcs-buffer
+        (apply 'start-process "working-copy-rcs" rcs-buffer command (cons "-t-wc-file" args)))
+
+      (pop-to-buffer rcs-buffer nil t) )))
+
+(defun wc-rcs-checkin ( log-message )
+  (wc-run-rcs-command "ci" `("-u" ,(concat "-m" log-message) ,(wc-local-file)))
+  (wc-run-rcs-command "co" `("-l" ,(wc-local-file))) )
+
+(defun wc-local-commit ( log-message )
+  (interactive "commit message: %s")
+  (catch 'abort
+    (unless log-message
+      (message "you did not supply a commit message")
+      (throw 'abort 't))
+
+    (wc-run-rcs-command "trucate" `("-s0" ,(wc-local-file)))
+
+    (wc-update-local-file)
+    (wc-rcs-checkin log-message) ))
+
+(defun wc-restore-from-bad-commit ()
+  (interactive)
   (wc-update-local-file)
+  (message "local file restored"))
+
+(defun wc-init-for-buffer ()
+  (interactive)
+
+  (wc-update-local-file)
+  (wc-rcs-checkin "first check-in")
+
   (wc-update-upstream-file)
   (wc-update-ancestor-file))
 
