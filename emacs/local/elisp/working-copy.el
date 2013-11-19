e7953232307a7f3f5247e8e6e0e3afe248dd0e34
Mike Mattie <codermattie@gmail.com>
2013-11-18 23:13:48 -0800

    proper working version of commit at least for working-copy

--- emacs/local/elisp/working-copy.el
+++ emacs/local/elisp/working-copy.el
@@ -12,6 +12,12 @@
 (defun wc-upstream-file ()
   (concat buffer-file-name ".upstream"))
 
+(defun wc-working-copy-file-p ( path )
+  (file-readable-p (concat path ".local")))
+
+(defun wc-working-copy-buffer-p ()
+  (file-readable-p (wc-local-file)))
+
 (defun wc-write-to-file ( path )
   (save-excursion
     (let
@@ -21,6 +27,15 @@
 
       (beginning-of-buffer)
       (write-region (point) end-of-file path) )))
+
+(defun wc-insert-from-file ( path )
+  (insert-file-contents path nil nil nil t))
+
+(defun wc-refresh-from-upstream ()
+  (wc-insert-from-file (wc-upstream-file)))
+
+(defun wc-refresh-from-local ()
+  (wc-insert-from-file (wc-local-file)))
 
 (defun wc-update-ancestor-file ()
   (wc-write-to-file (wc-ancestor-file)))
@@ -33,73 +48,125 @@
   (interactive)
   (wc-write-to-file (wc-upstream-file)))
 
-(defun wc-working-copy-file-p ( path )
-  (file-readable-p (concat path ".local")))
-
-(defun wc-working-copy-buffer-p ()
-  (file-readable-p (wc-local-file)))
-
 (defun wc-command-buffer ()
   (get-buffer-create "*working-copy-rcs*"))
 
 (defvar wc-wrappers-dir (concat grail-elisp-root "/wrappers/"))
 
-(defun wc-run-rcs-command ( command args )
-  (catch 'abort
-    (let
-      ((rcs-buffer (get-buffer-create "*wc rcs*"))
-       (old-buffer (current-buffer)))
+(defun wc-get-rcs-buffer ()
+  (get-buffer-create "*wc rcs*"))
 
-      (save-excursion
-        (with-current-buffer rcs-buffer
-          (apply 'start-process "working-copy-rcs" rcs-buffer command args))
-
-        (pop-to-buffer rcs-buffer nil t)
-        (other-window 1) )) ))
-
-(defun wc-rcs-start-for-buffer ()
+(defun wc-popup-rcs-buffer ()
   (let
-    ((check-in-dir (file-name-directory (wc-local-file))))
+    ((old-buffer (current-buffer)))
 
-    (unless (and check-in-dir (file-directory-p check-in-dir))
-      (let
-        ((rcs-storage (concat check-in-dir "/RCS")))
+    (pop-to-buffer (wc-get-rcs-buffer) nil t)
+    (other-window 1)
+    (switch-to-buffer old-buffer) ))
 
-        (unless (file-directory-p rcs-storage)
-          (make-directory rcs-storage)) )))
+(defun wc-build-rcs-command ( cmd next &rest args )
+  (lexical-let
+    ((full-args (cons cmd args))
+     (bind-next next))
 
-  (wc-rcs-checkin "first commit"))
+    `((lambda ()
+        (start-process-shell-command "wc-rcs" (wc-get-rcs-buffer) ,(string-join " " full-args)) )
 
-(defun wc-rcs-checkout-only ()
-  (wc-run-rcs-command "co" `("-l" ,(wc-local-file))) )
+       (lambda ()
+         (message "rcs command did not start!")
+         (wc-popup-rcs-buffer))
 
-(defun wc-rcs-checkin ( log-message )
-  (wc-run-rcs-command (concat wc-wrappers-dir "rcs-ci") `( ,(wc-local-file) ,log-message))
-  (wc-rcs-checkout-only))
+       (lambda ()
+         (message "rcs command returned error!")
+         (wc-popup-rcs-buffer))
 
-(defun wc-local-commit ( log-message )
-  (catch 'abort
-    (unless log-message
-      (message "you did not supply a commit message")
-      (throw 'abort 't))
+       (lambda ()
+         (message "rcs command completed.")
+         (wc-popup-rcs-buffer)
+         t)
+       ,bind-next) ))
 
-    (wc-run-rcs-command "trucate" `("-s0" ,(wc-local-file)))
+(defun wc-run-rcs-command ( constructed-command )
+  (wc-get-rcs-buffer)
+  (apply 'grail-process-async-chain constructed-command))
 
-    (wc-update-local-file)
-    (wc-rcs-checkin log-message) ))
+(defun wc-rcs-checkout ( file )
+  (wc-run-rcs-command
+    (wc-build-rcs-command "co" nil "-l" file)) )
 
-(defun wc-restore-from-bad-commit ()
+(defun wc-rcs-checkin ( file log-message )
+  (lexical-let
+    ((checkin-file file))
+
+    (wc-run-rcs-command
+      (wc-build-rcs-command
+        (concat wc-wrappers-dir "rcs-ci")
+        `(lambda ()
+           (wc-run-rcs-command
+             (wc-build-rcs-command "co" nil "-l" ,file)))
+        file log-message)) ))
+
+(defun wc-extract-log-from-buffer ()
+  (let
+    ((end-of-log (progn
+                   (end-of-buffer)
+                   (point)))
+     (start-of-log
+       (progn
+         (beginning-of-buffer)
+         (point))) )
+
+    (buffer-substring start-of-log end-of-log) ))
+
+(defun wc-commit-from-log-buffer ()
+  (let
+    ((log-buf (current-buffer)))
+
+    (when (wc-rcs-checkin file-to-commit (wc-extract-log-from-buffer))
+      (switch-to-buffer initiated-from-buffer)
+      (other-window 1)
+      (delete-other-windows)
+
+      (kill-buffer log-buf)) ))
+
+(defun wc-setup-log-buffer ()
+  (let
+    ((log-buffer (get-buffer-create "*rcs-log"))
+     (this-buffer (current-buffer)))
+
+    (with-current-buffer
+      (make-variable-buffer-local 'file-to-commit)
+      (make-variable-buffer-local 'initiated-from-buffer)
+
+      (setq file-to-commit (wc-local-file))
+      (setq initiated-from-buffer this-buffer)
+
+      (local-set-key (kbd "C-c C-c") 'wc-commmit-from-log-buffer))
+
+    log-buffer))
+
+(defun wc-commit-local ()
   (interactive)
-  (wc-update-local-file)
-  (message "local file restored"))
 
-(defun wc-init-for-buffer ()
+  (let
+    ((log-buffer (wc-setup-log-buffer)))
+
+    (pop-to-buffer log-buffer nil t)
+    (message "please enter a commit message followed by C-c C-c") ))
+
+(defun wc-commit ()
+  (interactive)
+
+  (wc-update-local-file)
+  (wc-commit-local) )
+
+(defun wc-init ()
   (interactive)
 
   (wc-update-local-file)
   (wc-update-upstream-file)
   (wc-update-ancestor-file)
 
-  (wc-rcs-start-for-buffer))
+  (wc-rcs-checkin (wc-local-file) "first commit"))
 
 (provide 'working-copy)
