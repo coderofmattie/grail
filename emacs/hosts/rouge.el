a31a89e0a3bd18c4455bae12237daba110e84244
Mike Mattie <codermattie@runbox.com>
2013-11-25 15:39:00 -0800

    have rouge use tags

--- emacs/hosts/rouge.el
+++ emacs/hosts/rouge.el
@@ -4,14 +4,19 @@
 ;; rouge system specific configuration
 ;;----------------------------------------------------------------------
 (require 'cm-string)
+
 (require 'file-utilities)
 (require 'perforce-utilities)
+(require 'tags-uber)
 
 (pforce-add-tree (files-make-path-rooted-to-home "work/import-codebase"))
 (pforce-add-tree (files-make-path-rooted-to-home "work/core-codebase"))
 (pforce-add-tree (files-make-path-rooted-to-home "work/import-codebase"))
 
 (perforce-activate-with-working-copy)
+
+(tags-uber-update-for-mode "cperl-mode"
+  '("inventory" "/home/codermattie/work/inventory-codebase"))
 
 ;; (require 'erc)
 ;; (require 'socks)
