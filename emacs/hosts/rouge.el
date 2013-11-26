0c954f2b6d06f069655a2adccbf3eb31354abcd7
Mike Mattie <codermattie@runbox.com>
2013-11-26 13:31:29 -0800

    add the full set of perl tags tables

--- emacs/hosts/rouge.el
+++ emacs/hosts/rouge.el
@@ -16,7 +16,21 @@
 (perforce-activate-with-working-copy)
 
 (tags-uber-update-for-mode "cperl-mode"
-  '("inventory" "/home/codermattie/work/inventory-codebase"))
+  '("inventory" "/home/codermattie/work/inventory-codebase")
+  '("system"
+     "/usr/local/lib/perl/5.14.2"
+     "/usr/local/share/perl/5.14.2"
+     "/usr/lib/perl5"
+     "/usr/share/perl5"
+     "/usr/lib/perl/5.14"
+     "/usr/share/perl/5.14"
+     "/usr/local/lib/site_perl")
+  '("cobalt-web"
+     "/web/lib/"
+     "/web/cobalt/"
+     "/web/auto/lib/"
+     "/web/auto/import/lib/"
+     "/web/auto/export/inventory/"))
 
 ;; (require 'erc)
 ;; (require 'socks)
