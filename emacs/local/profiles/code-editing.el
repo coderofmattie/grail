2842647643b84730ff4c27023ef8876c4986f529
Mike Mattie <codermattie@runbox.com>
2013-12-02 22:12:25 -0800

    start using git instead of unofficial repos

--- emacs/local/profiles/code-editing.el
+++ emacs/local/profiles/code-editing.el
@@ -1,10 +1,13 @@
 ;;----------------------------------------------------------------------
 ;; code editing tools
 ;;----------------------------------------------------------------------
+(grail-load 'dash (grail-define-installer "dash"
+                   "git"
+                   "https://github.com/magnars/dash.el.git"))
 
-(grail-load 'smartparens   (grail-define-installer "smartparens"
-                            "pkg"
-                            'smartparens))
+(grail-load 'smartparens (grail-define-installer "smartparens"
+                          "git"
+                          "https://github.com/Fuco1/smartparens.git"))
 
 (strip-minor-mode-keymap 'smartparens-mode)
 (set-face-background 'sp-pair-overlay-face "grey10")
@@ -13,9 +16,4 @@
 
 (defun procedural-smart-parens-editing ()
   (interactive)
-  (smartparens-mode 1) )
-
-
-
-
-
+  (smartparens-mode 1))
