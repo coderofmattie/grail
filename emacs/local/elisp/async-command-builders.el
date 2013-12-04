5a91c029be7b16acd06f477451c67666fe4d6d8b
Mike Mattie <codermattie@runbox.com>
2013-12-04 09:27:30 -0800

    async command has reached it's full potential with a macro for chaining

--- emacs/local/elisp/async-command-builders.el
+++ emacs/local/elisp/async-command-builders.el
@@ -2,15 +2,23 @@
 ;; async-command-builders.el
 ;;----------------------------------------------------------------------
 
-(defun async-build-basic ( prefix command callback &optional use-buffer )
+(defun async-build-chained-fn ( next-builder )
+  (lexical-let
+    ((builder next-builder))
+
+    (lambda ()
+      (apply 'grail-process-async-chain builder)) ))
+
+(defun async-build-basic ( prefix command callback &optional use-buffer &optional chained )
   (let
     ((grail-async-runner
        (lexical-let
          ((output-buffer (or use-buffer
                              (get-buffer-create (concat prefix "-proc"))))
-          (bind-command command)
-          (bind-prefix  prefix)
-          (bind-callback callback))
+          (bind-command  command)
+          (bind-prefix   prefix)
+          (bind-callback callback)
+          (bind-next     chained))
 
          `((lambda ()
              (start-process-shell-command ,bind-prefix ,output-buffer ,bind-command))
@@ -29,7 +37,22 @@
               (message "%s command %s completed." ,bind-prefix ,bind-command)
               (,bind-callback nil)
               t)
-            nil)) ))
+            ,bind-next)) ))
     grail-async-runner))
+
+(defmacro async-build-chained ( &rest body )
+  (let
+    ((this-builder nil))
+
+    (dolist ( builder (reverse body) )
+      (let
+        ((evaluated (mapcar 'eval builder)))
+
+        (setq this-builder
+          (if this-builder
+            (apply 'async-build-basic (append evaluated (list (async-build-chained-fn this-builder)) ))
+            (apply 'async-build-basic evaluated))) ))
+
+    `',this-builder))
 
 (provide 'async-command-builders)
