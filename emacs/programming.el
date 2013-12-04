e1166399bde97015a84dde4c179f2ba1cfecb1ba
Mike Mattie <codermattie@runbox.com>
2013-12-03 16:49:45 -0800

    *** empty log message ***

--- emacs/programming.el
+++ emacs/programming.el
@@ -8,6 +8,8 @@
 (require 'buffer-status)
 (require 'tags-uber)
 (require 'working-copy)
+(require 'version-control)
+(require 'merging)
 
 ;; re-usable programming modules
 (use-grail-profiles 0 "code-highlighting" "code-editing" "code-formatting" "repl")
@@ -62,60 +64,6 @@
 ;;----------------------------------------------------------------------
 (wc-enable-globally)
 
-;;----------------------------------------------------------------------
-;;                           Diff
-;;----------------------------------------------------------------------
-(require 'diff)
-
-(setq
-  diff-switches "-U3")                 ;; turn on standard context diffs,
-
-(add-hook 'diff-mode-hook
-
-  ;; when diff is called it will pop a window which is nice, but killing
-  ;; the buffer did not get rid of the popped window , until now.
-
-  (lambda ()
-    (add-hook 'kill-buffer-hook 'rid-window t t))
-  t)
-
-;;----------------------------------------------------------------------
-;;                          Ediff
-;;----------------------------------------------------------------------
-
-;; I will enventually create my interactive merge interface with this
-;; mode as a foundation. Until then tweak the basics.
-
-(require 'ediff)          ;; 2-3 way merge tool, can be used
-                          ;; for cherry picking and splitting
-
-(setq
-  ediff-custom-diff-options "-U3"         ;; same for ediff
-
-  ediff-split-window-function 'split-window-horizontally
-  ediff-merge-split-window-function 'split-window-horizontally
-
-  ediff-window-setup-function 'ediff-setup-windows-plain
-
-  ediff-auto-refine t)
-
-(defun teardown-ediff-after-merge ()
-  ;; cleanup ediff without asking or keeping the variants.
-  (let
-    ((merge ediff-buffer-C)
-     (panel (current-buffer)))
-
-    (ediff-janitor nil nil)
-
-    (switch-to-buffer merge)
-    (delete-other-windows)
-
-    (kill-buffer panel) ))
-
-(add-hook 'ediff-quit-hook 'teardown-ediff-after-merge t)
-
-(require 'external-merge-command)
-
 ;; this is insanely great. It displays the function you are "in" in terms
 ;; of the point. Really nice for reading long functions.
 
@@ -144,6 +92,8 @@
 
 (defun configure-for-programming ( list-fn-signatures &optional buffer-ring-mode )
   "Enable my programming customizations for the buffer"
+
+  (ver-ctl-bindings)
 
   ;; highlight all fucked up files.
   (whitespace-mode)
