56891026fdec871a2fded831b559d00a03aa950e
Mike Mattie <codermattie@runbox.com>
2014-07-03 00:38:31 -0700

    <keybindings> remove common functions for combining the setting of keybindings as it is now fanned out

--- emacs/programming.el
+++ emacs/programming.el
@@ -73,7 +73,7 @@
   (comment-or-uncomment-region (mark) (point)) )
 
 (defun toggle-comment-buffer ()
-  "toggle-comment-region
+  "toggle-comment-buffer
 
    comment or uncomment the region
   "
@@ -82,35 +82,9 @@
   (mark-whole-buffer)
   (call-interactively 'toggle-comment-region) )
 
-(defun configure-for-code ()
-  (custom-key-group "code editing" "c" nil
-    ("c" . 'toggle-comment-region)
-    ("b" . 'toggle-comment-buffer)
-    ("i" . 'indent-region)
-    ("s" . 'sort-lines)) )
-
-;; (defun configure-for-select ( select-inner select-outer )
-;;   (local-set-key (kbd "C-c s w")  'select-word)
-
-;;   (local-set-key (kbd "C-c s i")  select-inner)
-;;   (local-set-key (kbd "C-c s o")  select-outer))
-
 (defun configure-for-search-dummy ()
   (interactive)
   (message "function signature to find is not specified"))
-
-(defun configure-for-search ( signatures-fn )
-  (let
-    ((fn-function (or signatures-fn 'configure-for-search-dummy)))
-
-    (custom-key-group "search" "s" nil
-      ("f" . fn-function)
-      ("g" . 'grep)
-      ("r" . 'rgrep)
-      ("o" . 'occur)
-;;      ("i" . 'tags-uber-incremental)
-;;      ("t" . 'tags-uber-search)
-      ) ))
 
 (defvar configure-programming-hook nil)
 
@@ -125,12 +99,27 @@
   (run-custom-hooks configure-programming-hook)
 
   (configure-for-buffer-ring buffer-ring-mode)
-  (configure-for-search list-fn-signatures)
 
   ;; better return key for programming
   (local-set-key (kbd "<return>") 'newline-and-indent)
 
-  (configure-for-code)
+  (let
+    ((fn-search (or list-fn-signatures 'configure-for-search-dummy)))
+
+    (custom-key-group "search" "s" nil
+      ("f" . fn-search)
+      ("g" . grep)
+      ("r" . rgrep)
+      ("o" . occur)
+;;    ("i" . 'tags-uber-incremental)
+;;    ("t" . 'tags-uber-search)
+      ) )
+
+  (custom-key-group "code editing" "c" nil
+    ("c" . toggle-comment-region)
+    ("b" . toggle-comment-buffer)
+    ("i" . indent-region)
+    ("s" . sort-lines) )
 
   ;; found this on emacs-wiki , all scripts are automatically made executable.
   (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p t) )
@@ -138,28 +127,6 @@
 (defun configure-for-navigation ( forwards backwards )
   (local-set-key (kbd "M-f") forwards)
   (local-set-key (kbd "M-b") backwards))
-
-(defun configure-for-evaluation ( eval-define eval-expression eval-region eval-buffer )
-  "Enable my programming customizations for the buffer
-
-   eval-def     : this should evaluate definitions, variables,constants, and functions.
-   eval-expr    : evaluate the current or last expression
-   eval-region  : evaluate the region
-   eval-buffer  : evaluate the buffer
-  "
-  (custom-key-group "search" "e" nil
-    ("d" . eval-define)
-    ("e" . eval-expression)
-    ("r" . eval-region)
-    ("b" . eval-buffer)) )
-
-(defun configure-for-docs ( docs-browse )
-  (custom-key-group "search" "i" nil
-    ("b" . docs-browse)) )
-
-(defun configure-for-debugging ( eval-debug )
-  (custom-key-group "search" "d" nil
-    ("d" . eval-debug)) )
 
 (defun configure-for-macros ( expand-macro )
   (local-set-key (kbd "C-c m e") expand-macro) )
