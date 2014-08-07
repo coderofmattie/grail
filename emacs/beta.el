d8cc79257e5222eb3a2320efbaf5d03a19f844b8
Mike Mattie <codermattie@runbox.com>
2014-08-06 17:47:31 -0700

    <refactor> move commands to keep out of beta and the rest can be            removed when it is checked that the functions are no            longer used anywhere

--- emacs/beta.el
+++ emacs/beta.el
@@ -67,35 +67,6 @@
       nil
       t)))
 
-;;----------------------------------------------------------------------
-;; experimental - interesting
-;;----------------------------------------------------------------------
-
-;; these only work on X, when they work on the mac they can go into stable.el.
-
-(defun maximize-frame ()
-  "toggle maximization the current frame"
-  (interactive)
-  (cond
-    ((eq 'x (window-system))
-
-      (progn
-        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-          '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
-
-        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-          '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0)) ))
-
-    ((eq 'w32 (window-system))
-      (w32-send-sys-command 61488))
-
-    ((message "window system %s is not supported by maximize" (symbol-name (window-system)))) ))
-
-(defun fullscreen ()
-  (interactive)
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-    '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
-
 (defun elisp-in-path ( path path-type )
   "return a list of elisp files in the path"
 
--- emacs/commands.el
+++ emacs/commands.el
@@ -83,3 +83,27 @@
   (interactive)
   (x-select-text (filter-buffer-substring (region-beginning) (region-end))) )
 
+(defun maximize ()
+  "toggle maximization the current frame"
+  (interactive)
+  (cond
+    ((eq 'x (window-system))
+
+      (progn
+        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+          '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+
+        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+          '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0)) ))
+
+    ((eq 'w32 (window-system))
+      (w32-send-sys-command 61488))
+
+    ((message "window system %s is not supported by maximize" (symbol-name (window-system)))) ))
+
+(defun fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+    '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+
+
