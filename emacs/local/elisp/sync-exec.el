5e3b15a782043f4b73cf89dce08e8916fb620a73
Mike Mattie <codermattie@runbox.com>
2014-08-12 04:12:16 -0700

    <sync-exec> revisions from grail-profile work. put the call-process             directly inside a let setting the default dir because let             is not acting very dynamic

--- emacs/local/elisp/sync-exec.el
+++ emacs/local/elisp/sync-exec.el
@@ -11,8 +11,10 @@
   (let
     ((exec-buffer (get-buffer-create buffer-name)))
 
-    (with-current-buffer exec-buffer
-      (term-mode))
+;; maybe possible to create a more terminal like buffer
+
+;;    (with-current-buffer exec-buffer
+;;      (term-mode))
 
     exec-buffer))
 
@@ -20,7 +22,16 @@
   (with-current-buffer buffer
     (erase-buffer)) )
 
-(defun sync-exec ( exec-buffer command &rest args )
-  (apply 'call-process command nil exec-buffer t args) )
+(defun sync-exec-args ( dir exec-buffer command &rest args )
+  (let
+    ((default-directory dir))
+
+    (apply 'call-process command nil exec-buffer t args) ))
+
+(defun sync-exec-list ( dir exec-buffer command-list )
+  (let
+    ((default-directory (concat dir "/") ))
+
+    (apply 'call-process (car command-list) nil exec-buffer t (cdr command-list)) ))
 
 (provide 'sync-exec)
