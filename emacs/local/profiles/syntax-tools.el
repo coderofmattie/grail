757e66bfeb71660628f27a7a782caa47a7f59611
Mike Mattie <codermattie@runbox.com>
2014-08-18 00:10:18 -0700

    <syntax-tools> create a profile based largely on syntax-move which                intergates third party syntax support

--- emacs/local/profiles/syntax-tools.el
+++ emacs/local/profiles/syntax-tools.el
@@ -1,11 +1,16 @@
 ;;----------------------------------------------------------------------
-;; code editing tools
+;; syntax-tools
 ;;----------------------------------------------------------------------
 (require 'mode-tools)
+(require 'syntax-move)
 
 (grail-load-package 'dash "git" "https://github.com/magnars/dash.el.git")
 
 (grail-load-package 'smartparens "git" "https://github.com/Fuco1/smartparens.git")
+
+;;
+;; adapt smartparens
+;;
 
 (strip-minor-mode-keymap 'smartparens-mode)
 
@@ -13,8 +18,51 @@
 (set-face-background 'sp-wrap-overlay-face "grey10")
 (set-face-background 'sp-wrap-tag-overlay-face "grey10")
 
-(defun profile/syntax-tools-setup ()
+(setq sp-navigate-reindent-after-up nil)
+
+;;
+;; create adapter to remap parens with smartparens in the mix
+;;
+
+(defun insert-smart-remapped-char ( character )
+  (setq last-command-event character)
+  (sp--self-insert-command 1) )
+
+(defun insert-dumb-remapped-char ( character )
+  (insert-char character)
+  (self-insert-command 0) )
+
+(defun profile/syntax-tools-lisp ()
+  "bind the parentheses to the brace keys, while the shifted
+   paren keys become the braces."
   (interactive)
-  (smartparens-mode 1))
+
+  ;;
+  ;; swap parens keys
+  ;;
+
+  (local-set-key (kbd "[") (lambda () (interactive) (insert-smart-remapped-char ?\( )) )
+  (local-set-key (kbd "]") (lambda () (interactive) (insert-dumb-remapped-char ?\) )) )
+
+  (local-set-key (kbd "(") (lambda () (interactive) (insert-smart-remapped-char ?\[)) )
+  (local-set-key (kbd ")") (lambda () (interactive) (insert-dumb-remapped-char ?\])) )
+
+  ;;
+  ;; use syntatic move bindings for lisp
+  ;;
+
+  (syntax-move/bind-prev       'sp-next-sexp)
+  (syntax-move/bind-next       'sp-prev-sexp)
+  (syntax-move/bind-opening    'sp-beginning-of-sexp)
+  (syntax-move/bind-closing    'sp-end-of-sexp)
+  (syntax-move/bind-up-scope   'sp-up-sexp)
+  (syntax-move/bind-down-scope 'sp-down-sexp)
+  (syntax-move/bind-kill-expr  'sp-sexp)
+
+  (syntax-move/local-keybindings) )
+
+(defun profile/syntax-tools-mode-setup ()
+  (interactive)
+  (smartparens-mode 1) )
 
 (provide 'profile/syntax-tools)
