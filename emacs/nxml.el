020b9c83e31ba7387c45e8e1a39000c924ef693a
Mike Mattie <codermattie@runbox.com>
2014-08-18 00:30:08 -0700

    <nxml> replace stale nxml configure-for-buffer-ring with new buffer        ring init

--- emacs/nxml.el
+++ emacs/nxml.el
@@ -8,10 +8,15 @@
                                  ("\\.xml$"     . nxml-mode)
                                  ) auto-mode-alist ))
 
-(add-hook 'nxml-mode-hook
-  (lambda ()
-    (dwim-tab-localize-context (dwim-tab-make-expander 'dwim-tab-stem-trigger 'nxml-complete))
-    (turn-on-dwim-tab 'nxml-indent-line)
-    (configure-for-buffer-ring "nxml-mode"))
-  t)
+(defun nxml-configure-for-mode ()
+  (buffer-ring/add "nxml-mode")
+  (buffer-ring/local-keybindings)
+
+  (dwim-tab-localize-context (dwim-tab-make-expander
+                               'dwim-tab-stem-trigger
+                               'nxml-complete))
+
+  (turn-on-dwim-tab 'nxml-indent-line) )
+
+(add-hook 'nxml-mode-hook 'nxml-configure-for-mode t)
 
