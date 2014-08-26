f010dae32cab4143bde1f5177ffc87fdbbe30f31
Mike Mattie <codermattie@runbox.com>
2014-08-25 23:35:53 -0700

    <fix> update the port for socks5 proxy

--- emacs/hosts/khan.el
+++ emacs/hosts/khan.el
@@ -6,6 +6,6 @@
 
 (require 'socks)
 
-(setq socks-server (list "priv" "localhost" 6666 5))
+(setq socks-server (list "priv" "localhost" 9999 5))
 
 (setq erc-server-connect-function 'socks-open-network-stream)
